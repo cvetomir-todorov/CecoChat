e3719ad99540834fb1b4fb5e4251f87827eb3177
ceco <cvetomir.todorov@gmail.com>
2021-01-17 17:22:26 +0200

    Use Serilog for server logging

--- source/CecoChat.Messaging.Server/Program.cs
+++ source/CecoChat.Messaging.Server/Program.cs
@@ -1,5 +1,10 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace CecoChat.Messaging.Server
 {
@@ -7,13 +12,49 @@ namespace CecoChat.Messaging.Server
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            SetupLogging();
+            ILogger logger = Log.ForContext(typeof(Program));
+
+            try
+            {
+                logger.Information("Starting...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Fatal(exception, "Unexpected failure.");
+            }
+            finally
+            {
+                logger.Information("End.");
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
+        private static void SetupLogging()
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            string binPath = Path.GetDirectoryName(currentAssembly.Location);
+            Environment.SetEnvironmentVariable("CecoChatLoggingDir", binPath);
+
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("logging.json", optional: false)
+                .AddJsonFile($"logging.{environment}.json", optional: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.WithProperty("Application", currentAssembly.GetName().Name)
+                .CreateLogger();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host
                 .CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
