07d4fc1f2dc28add6d8f1901f6194487bc0dc663
ceco <cvetomir.todorov@gmail.com>
2021-01-17 12:22:17 +0200

    Add kafka configuration

---
+++ source/CecoChat.Messaging.Server/Servers/KafkaOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CecoChat.Messaging.Server.Servers
+{
+    public interface IKafkaOptions
+    {
+        public List<string> BootstrapServers { get; }
+
+        public string MessagesTopic { get; }
+
+        public int MessagesTopicPartitionCount { get; }
+
+        public string ConsumerGroupID { get; }
+    }
+
+    public sealed class KafkaOptions : IKafkaOptions
+    {
+        public List<string> BootstrapServers { get; set; }
+
+        public string MessagesTopic { get; set; }
+
+        public int MessagesTopicPartitionCount { get; set; }
+
+        public string ConsumerGroupID { get; set; }
+    }
+}
