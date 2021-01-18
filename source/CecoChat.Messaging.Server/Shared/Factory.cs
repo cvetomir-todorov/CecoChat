2ee2cbd29668a6169f80fc0844276c6db3ef431c
ceco <cvetomir.todorov@gmail.com>
2021-01-18 14:29:18 +0200

    Support registering factories

---
+++ source/CecoChat.Messaging.Server/Shared/Factory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CecoChat.Messaging.Server.Shared
+{
+    public interface IFactory<out TService>
+    {
+        TService Create();
+    }
+
+    public sealed class Factory<TService> : IFactory<TService>
+    {
+        private readonly Func<TService> _createFunction;
+
+        public Factory(Func<TService> createFunction)
+        {
+            _createFunction = createFunction;
+        }
+
+        public TService Create()
+        {
+            return _createFunction();
+        }
+    }
+
+    public static class FactoryExtensions
+    {
+        public static void AddFactory<TService, TImplementation>(this IServiceCollection services)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            services.AddTransient<TService, TImplementation>();
+            services.AddSingleton<Func<TService>>(serviceProvider => () => serviceProvider.GetRequiredService<TService>());
+            services.AddSingleton<IFactory<TService>, Factory<TService>>();
+        }
+    }
+}
