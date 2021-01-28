6bede87efbd5876ba2b37e5b8bf1b9d51e17c095
ceco <cvetomir.todorov@gmail.com>
2021-01-28 11:47:12 +0200

    Scroll down to the latest sent/received message in chat window

---
+++ source/CecoChat.Client.Wpf/Infrastructure/ScrollIntoViewBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Specialized;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace CecoChat.Client.Wpf.Infrastructure
+{
+    public sealed class ScrollIntoViewBehavior : Behavior<ListView>
+    {
+        protected override void OnAttached()
+        {
+            INotifyCollectionChanged items = AssociatedObject.Items;
+            items.CollectionChanged += ListViewOnCollectionChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            INotifyCollectionChanged items = AssociatedObject.Items;
+            items.CollectionChanged -= ListViewOnCollectionChanged;
+        }
+
+        private void ListViewOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems != null)
+            {
+                // ReSharper disable once AssignNullToNotNullAttribute
+                AssociatedObject.ScrollIntoView(e.NewItems[^1]);
+            }
+        }
+    }
+}
