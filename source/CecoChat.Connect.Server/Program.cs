757d74cc6a8e9e2e51e983c716160d4fb65ee684
ceco <cvetomir.todorov@gmail.com>
2021-02-02 15:36:19 +0200

    Add connect server base implementation

---
+++ source/CecoChat.Connect.Server/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Reflection;
+using CecoChat.Serilog;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace CecoChat.Connect.Server
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            SerilogConfiguration.Setup(Assembly.GetEntryAssembly());
+            ILogger logger = Log.ForContext(typeof(Program));
+
+            try
+            {
+                logger.Information("Starting...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Fatal(exception, "Unexpected failure.");
+            }
+            finally
+            {
+                logger.Information("Ended.");
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host
+                .CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
+    }
+}
