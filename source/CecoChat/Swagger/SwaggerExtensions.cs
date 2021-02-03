bc6cfac84c34b95eb6d2e5694f451e31e3748233
ceco <cvetomir.todorov@gmail.com>
2021-02-03 21:44:14 +0200

    Add Swagger functionality to base project

---
+++ source/CecoChat/Swagger/SwaggerExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CecoChat.Swagger
+{
+    public static class SwaggerExtensions
+    {
+        public static void AddSwaggerServices(this IServiceCollection services, ISwaggerOptions options)
+        {
+            if (options.UseSwagger)
+            {
+                services.AddSwaggerGen(config =>
+                {
+                    config.SwaggerDoc(options.OpenApiInfo.Version, options.OpenApiInfo);
+                });
+                services.ConfigureSwaggerGen(config =>
+                {
+                    config.CustomSchemaIds(type => type.FullName);
+                });
+            }
+        }
+
+        public static void UseSwaggerMiddlewares(this IApplicationBuilder app, ISwaggerOptions options)
+        {
+            if (options.UseSwagger)
+            {
+                app.UseSwagger();
+            }
+
+            if (options.UseSwaggerUI)
+            {
+                app.UseSwaggerUI(config => config.SwaggerEndpoint(
+                    url: options.Url,
+                    name: $"{options.OpenApiInfo.Title} {options.OpenApiInfo.Version}"));
+            }
+        }
+
+	}
+}
---
+++ source/CecoChat/Swagger/SwaggerOptions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace CecoChat.Swagger
+{
+    public interface ISwaggerOptions
+    {
+        bool UseSwagger { get; }
+
+        bool UseSwaggerUI { get; }
+
+        string Url { get; }
+
+        OpenApiInfo OpenApiInfo { get; }
+    }
+
+    public sealed class SwaggerOptions : ISwaggerOptions
+    {
+        public bool UseSwagger { get; set; }
+
+        public bool UseSwaggerUI { get; set; }
+
+        public string Url { get; set; }
+
+        public OpenApiInfo OpenApiInfo { get; set; }
+    }
+}
