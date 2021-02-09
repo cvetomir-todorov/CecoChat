8d6780c66833da6be44ef74c493496cec3a1c57b
ceco <cvetomir.todorov@gmail.com>
2021-02-09 17:32:24 +0200

    Fix profile server containerize files

--- containerize/BuildImageForProfile.ps1
+++ containerize/BuildImageForProfile.ps1
@@ -1,2 +1,2 @@
-dotnet publish ..\source\CecoChat.Server.Profile\CecoChat.Server.Profile.csproj -o .\Profile -c Debug
+dotnet publish ..\source\CecoChat.Profile.Server\CecoChat.Profile.Server.csproj -o .\Profile -c Debug
 docker build -f .\Profile.Dockerfile -t ceco.com/cecochat/profile:0.1 .
--- containerize/Profile.Dockerfile
+++ containerize/Profile.Dockerfile
@@ -1,4 +1,4 @@
 FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS runtime
 WORKDIR /app
 COPY Profile/. ./
-ENTRYPOINT ["dotnet", "CecoChat.Server.Profile.dll"]
+ENTRYPOINT ["dotnet", "CecoChat.Profile.Server.dll"]
