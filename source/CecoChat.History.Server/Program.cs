60916c3fd9a4dc276f8ef2d41deab98d52266805
ceco <cvetomir.todorov@gmail.com>
2021-03-21 16:23:29 +0200

    Reuse entry point code

--- source/CecoChat.History.Server/Program.cs
+++ source/CecoChat.History.Server/Program.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Reflection;
-using CecoChat.Serilog;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using CecoChat.Server;
 using Microsoft.Extensions.Hosting;
-using Serilog;
 
 namespace CecoChat.History.Server
 {
@@ -12,38 +7,8 @@ namespace CecoChat.History.Server
     {
         public static void Main(string[] args)
         {
-            SerilogConfiguration.Setup(Assembly.GetEntryAssembly());
-            ILogger logger = Log.ForContext(typeof(Program));
-
-            try
-            {
-                logger.Information("Starting...");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception exception)
-            {
-                logger.Fatal(exception, "Unexpected failure.");
-            }
-            finally
-            {
-                logger.Information("Ended.");
-                Log.CloseAndFlush();
-            }
-        }
-
-        private static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host
-                .CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureAppConfiguration(configurationBuilder =>
-                {
-                    configurationBuilder.AddEnvironmentVariables(prefix: "CECOCHAT_");
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            IHostBuilder hostBuilder = EntryPoint.CreateDefaultHostBuilder(args, startupContext: typeof(Startup));
+            EntryPoint.CreateAndRunHost(hostBuilder, loggerContext: typeof(Program));
         }
     }
 }
---
+++ source/CecoChat.Server/EntryPoint.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+using CecoChat.Serilog;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace CecoChat.Server
+{
+    public static class EntryPoint
+    {
+        public static IHostBuilder CreateDefaultHostBuilder(
+            string[] args,
+            Type startupContext,
+            bool useSerilog = true,
+            string environmentVariablesPrefix = "CECOCHAT_")
+        {
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+
+            if (startupContext != null)
+            {
+                hostBuilder = hostBuilder.ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup(startupContext);
+                });
+            }
+            if (useSerilog)
+            {
+                hostBuilder = hostBuilder.UseSerilog();
+            }
+            if (!string.IsNullOrWhiteSpace(environmentVariablesPrefix))
+            {
+                hostBuilder = hostBuilder.ConfigureAppConfiguration(configurationBuilder =>
+                {
+                    configurationBuilder.AddEnvironmentVariables(environmentVariablesPrefix);
+                });
+            }
+
+            return hostBuilder;
+        }
+
+        public static void CreateAndRunHost(IHostBuilder hostBuilder, Type loggerContext)
+        {
+            SerilogConfiguration.Setup(Assembly.GetEntryAssembly());
+            ILogger logger = Log.ForContext(loggerContext);
+
+            try
+            {
+                logger.Information("Starting...");
+                hostBuilder.Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Fatal(exception, "Unexpected failure.");
+            }
+            finally
+            {
+                logger.Information("Ended.");
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
