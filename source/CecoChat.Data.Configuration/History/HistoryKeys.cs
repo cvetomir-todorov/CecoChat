c353bb20e63d2c65b1a9b04ccb43fcb1ad778bd4
ceco <cvetomir.todorov@gmail.com>
2021-03-22 11:22:55 +0200

    Validate history configuration - server address and message count usage

--- source/CecoChat.Data.Configuration/History/HistoryKeys.cs
+++ source/CecoChat.Data.Configuration/History/HistoryKeys.cs
@@ -5,5 +5,6 @@
         public const string ServerAddress = "history.server-address";
         public const string UserMessageCount = "history.user.message-count";
         public const string DialogMessageCount = "history.dialog.message-count";
+        public const string MessageCount = "history.message-count";
     }
 }
---
+++ source/CecoChat.Data.Configuration/History/IHistoryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace CecoChat.Data.Configuration.History
+{
+    public sealed class HistoryConfigurationUsage
+    {
+        public bool UseServerAddress { get; init; }
+
+        public bool UseMessageCount { get; init; }
+    }
+
+    public interface IHistoryConfiguration
+    {
+        Task Initialize(HistoryConfigurationUsage usage);
+
+        string ServerAddress { get; }
+
+        int UserMessageCount { get; }
+
+        int DialogMessageCount { get; }
+    }
+}
