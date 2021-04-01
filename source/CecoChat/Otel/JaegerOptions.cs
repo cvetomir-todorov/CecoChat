e9189669496969d9afdbd4a76531bb3a1e71f51c
ceco <cvetomir.todorov@gmail.com>
2021-04-01 17:26:21 +0300

    Add OpenTelemetry configuration and registration API

---
+++ source/CecoChat/Otel/JaegerOptions.cs
@@ -0,0 +1,26 @@
+﻿using OpenTelemetry;
+
+namespace CecoChat.Otel
+{
+    public interface IJaegerOptions
+    {
+        string AgentHost { get; }
+
+        int AgentPort { get; }
+
+        ExportProcessorType ExportProcessorType { get; }
+
+        int BatchExportScheduledDelayMillis{ get; }
+    }
+
+    public sealed class JaegerOptions : IJaegerOptions
+    {
+        public string AgentHost { get; set; }
+
+        public int AgentPort { get; set; }
+
+        public ExportProcessorType ExportProcessorType { get; set; }
+
+        public int BatchExportScheduledDelayMillis { get; set; }
+    }
+}
