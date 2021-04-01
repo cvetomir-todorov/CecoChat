d5c7695623bfd7bf18ea9c94d4d0557681046605
ceco <cvetomir.todorov@gmail.com>
2021-04-01 17:26:21 +0300

    Add OpenTelemetry configuration and registration API

---
+++ source/CecoChat/Otel/OtelExtensions.cs
@@ -0,0 +1,38 @@
+﻿using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace CecoChat.Otel
+{
+    public record OtelServiceResource
+    {
+        public string Namespace { get; init; }
+        public string Service { get; init; }
+        public string Version { get; init; }
+    }
+
+    public static class OtelExtensions
+    {
+        public static TracerProviderBuilder AddServiceResource(this TracerProviderBuilder otel, OtelServiceResource serviceResource)
+        {
+            ResourceBuilder resourceBuilder = ResourceBuilder.CreateDefault()
+                .AddService(
+                    serviceNamespace: serviceResource.Namespace,
+                    serviceName: serviceResource.Service,
+                    serviceVersion: serviceResource.Version);
+
+            return otel.SetResourceBuilder(resourceBuilder);
+        }
+
+        public static TracerProviderBuilder ConfigureJaegerExporter(this TracerProviderBuilder otel, IJaegerOptions options)
+        {
+            return otel
+                .AddJaegerExporter(config =>
+                {
+                    config.AgentHost = options.AgentHost;
+                    config.AgentPort = options.AgentPort;
+                    config.ExportProcessorType = options.ExportProcessorType;
+                    config.BatchExportProcessorOptions.ScheduledDelayMilliseconds = options.BatchExportScheduledDelayMillis;
+                });
+        }
+    }
+}
