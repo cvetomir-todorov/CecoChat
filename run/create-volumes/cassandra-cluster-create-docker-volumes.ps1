3c6987fed4f124257d7e3a34778d02e88fc27033
ceco <cvetomir.todorov@gmail.com>
2021-04-02 11:57:13 +0300

    Refactor docker compose files: - Remove the 3rd cassandra instance, leaving just 2 - Extract management containers into separate files - Shorten file names - Adjust memory limits

--- run/create-volumes/cassandra-cluster-create-docker-volumes.ps1
+++ run/create-volumes/cassandra-cluster-create-docker-volumes.ps1
@@ -2,5 +2,3 @@ docker volume create cecochat-cass1-data
 docker volume create cecochat-cass1-conf
 docker volume create cecochat-cass2-data
 docker volume create cecochat-cass2-conf
-docker volume create cecochat-cass3-data
-docker volume create cecochat-cass3-conf
