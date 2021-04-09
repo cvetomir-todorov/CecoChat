35c2e1bc28a5ac784dbbd7e55d5d69a056d200f8
ceco <cvetomir.todorov@gmail.com>
2021-04-09 21:46:24 +0300

    Put serilog configuration in code

--- source/CecoChat/Serilog/SerilogConfiguration.cs
+++ source/CecoChat/Serilog/SerilogConfiguration.cs
@@ -1,35 +1,69 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
-using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Enrichers.Span;
+using Serilog.Events;
+using Serilog.Formatting.Elasticsearch;
 
 namespace CecoChat.Serilog
 {
     public static class SerilogConfiguration
     {
-        public static void Setup(Assembly entryAssembly)
+        public static void Setup(Assembly entryAssembly, string environment)
         {
-            string binPath = Path.GetDirectoryName(entryAssembly.Location);
-            Environment.SetEnvironmentVariable("CecoChatLoggingDir", binPath);
+            LoggerConfiguration configuration = CreateDefaultConfiguration(entryAssembly);
 
-            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
-                .AddJsonFile("logging.json", optional: false);
-
-            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            if (!string.IsNullOrWhiteSpace(environment))
+            switch (environment.ToLowerInvariant())
             {
-                configurationBuilder.AddJsonFile($"logging.{environment}.json", optional: true);
+                case "development":
+                    configuration = ApplyDevelopmentConfiguration(entryAssembly, configuration);
+                    break;
+                case "production":
+                    configuration = ApplyProductionConfiguration(configuration);
+                    break;
+                default:
+                    throw new InvalidOperationException($"Logging configuration doesn't support environment '{environment}'.");
             }
 
-            IConfiguration configuration = configurationBuilder.Build();
+            Log.Logger = configuration.CreateLogger();
+        }
 
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
+        private static LoggerConfiguration CreateDefaultConfiguration(Assembly entryAssembly)
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Is(LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                .Enrich.WithMachineName()
                 .Enrich.WithProperty("Application", entryAssembly.GetName().Name)
                 .Enrich.WithSpan()
-                .CreateLogger();
+                .Enrich.FromLogContext()
+                .Destructure.ToMaximumDepth(4)
+                .Destructure.ToMaximumStringLength(1024)
+                .Destructure.ToMaximumCollectionCount(32);
+        }
+
+        private static LoggerConfiguration ApplyDevelopmentConfiguration(Assembly entryAssembly, LoggerConfiguration configuration)
+        {
+            string name = entryAssembly.GetName().Name;
+            string binPath = Path.GetDirectoryName(entryAssembly.Location) ?? Environment.CurrentDirectory;
+            // going from /source/project/bin/debug/.net5.0/ to /source/logs/project.txt
+            string filePath = Path.Combine(binPath, "..", "..", "..", "..", "logs", $"{name}.txt");
+
+            return configuration
+                .MinimumLevel.Override("CecoChat", LogEventLevel.Verbose)
+                .WriteTo.Console()
+                .WriteTo.File(
+                    path: filePath,
+                    rollingInterval: RollingInterval.Day,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss zzz} | {Level:u3} | {SourceContext} | {Message:lj}{NewLine}{Exception}");
+        }
+
+        private static LoggerConfiguration ApplyProductionConfiguration(LoggerConfiguration configuration)
+        {
+            return configuration
+                .WriteTo.Console(new ElasticsearchJsonFormatter(renderMessageTemplate: false));
         }
     }
 }
