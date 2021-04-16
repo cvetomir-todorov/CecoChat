3fbbd6aeab0cf2e1008e18ad42f4894a18dd2669
ceco <cvetomir.todorov@gmail.com>
2021-04-16 18:03:48 +0300

    Use the cancellation token provided by the host application lifetime in addition to the one provided by the hosted service

--- source/CecoChat.Materialize.Server/Initialization/MaterializeMessagesHostedService.cs
+++ source/CecoChat.Materialize.Server/Initialization/MaterializeMessagesHostedService.cs
@@ -7,33 +7,45 @@ using Microsoft.Extensions.Logging;
 
 namespace CecoChat.Materialize.Server.Initialization
 {
-    public class MaterializeMessagesHostedService : IHostedService
+    public class MaterializeMessagesHostedService : IHostedService, IDisposable
     {
         private readonly ILogger _logger;
         private readonly IMaterializeMessagesConsumer _materializeMessagesConsumer;
+        private readonly CancellationToken _appStoppingCt;
+        private CancellationTokenSource _stoppedCts;
 
         public MaterializeMessagesHostedService(
             ILogger<MaterializeMessagesHostedService> logger,
+            IHostApplicationLifetime applicationLifetime,
             IMaterializeMessagesConsumer materializeMessagesConsumer)
         {
             _logger = logger;
             _materializeMessagesConsumer = materializeMessagesConsumer;
+
+            _appStoppingCt = applicationLifetime.ApplicationStopping;
+        }
+
+        public void Dispose()
+        {
+            _stoppedCts?.Dispose();
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
+            _stoppedCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _appStoppingCt);
+
             _materializeMessagesConsumer.Prepare();
             Task.Factory.StartNew(() =>
             {
                 try
                 {
-                    _materializeMessagesConsumer.Start(cancellationToken);
+                    _materializeMessagesConsumer.Start(_stoppedCts.Token);
                 }
                 catch (Exception exception)
                 {
                     _logger.LogCritical(exception, "Failure in materialize messages consumer.");
                 }
-            }, cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Current);
+            }, _stoppedCts.Token, TaskCreationOptions.LongRunning, TaskScheduler.Current);
 
             return Task.CompletedTask;
         }
