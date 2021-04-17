1d98fa072846246f584f9c361b6598c484e53d7c
ceco <cvetomir.todorov@gmail.com>
2021-04-17 12:25:55 +0300

    Prepare queries in the background and keep loading the application

--- source/CecoChat.Materialize.Server/Initialization/PrepareQueriesHostedService.cs
+++ source/CecoChat.Materialize.Server/Initialization/PrepareQueriesHostedService.cs
@@ -1,23 +1,41 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using CecoChat.Data.History;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace CecoChat.Materialize.Server.Initialization
 {
     public sealed class PrepareQueriesHostedService : IHostedService
     {
+        private readonly ILogger _logger;
         private readonly INewMessageRepository _newMessageRepository;
 
         public PrepareQueriesHostedService(
+            ILogger<PrepareQueriesHostedService> logger,
             INewMessageRepository newMessageRepository)
         {
+            _logger = logger;
             _newMessageRepository = newMessageRepository;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _newMessageRepository.Prepare();
+            Task.Run(() =>
+            {
+                try
+                {
+                    _logger.LogInformation("Start preparing queries...");
+                    _newMessageRepository.Prepare();
+                    _logger.LogInformation("Completed preparing queries.");
+                }
+                catch (Exception exception)
+                {
+                    _logger.LogCritical(exception, "Failed to prepare queries.");
+                }
+            }, cancellationToken);
+
             return Task.CompletedTask;
         }
 
