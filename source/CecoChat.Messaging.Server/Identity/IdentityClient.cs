390dcfae6f4d009130d43c04f5f9a07b4d13f272
ceco <cvetomir.todorov@gmail.com>
2021-04-19 19:08:13 +0300

    Make identity client more performant and resilient

--- source/CecoChat.Messaging.Server/Identity/IdentityClient.cs
+++ source/CecoChat.Messaging.Server/Identity/IdentityClient.cs
@@ -1,26 +1,77 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using CecoChat.Contracts.Identity;
+using Grpc.Core;
 using Grpc.Net.Client;
+using Grpc.Net.Client.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace CecoChat.Messaging.Server.Identity
 {
     public interface IIdentityClient : IDisposable
     {
-        Task<long> GenerateIdentity(long userID);
+        Task<GenerateIdentityResult> GenerateIdentity(long userID);
+    }
+
+    public struct GenerateIdentityResult
+    {
+        public bool Success { get; set; }
+        public long ID { get; set; }
     }
 
     public sealed class IdentityClient : IIdentityClient
     {
+        private readonly ILogger _logger;
+        private readonly IIdentityOptions _options;
         private readonly GrpcChannel _channel;
         private readonly Contracts.Identity.Identity.IdentityClient _client;
 
         public IdentityClient(
+            ILogger<IdentityClient> logger,
+            ILoggerFactory loggerFactory,
             IOptions<IdentityOptions> options)
         {
-            _channel = GrpcChannel.ForAddress(options.Value.Address);
+            _logger = logger;
+            _options = options.Value;
+            _channel = CreateChannel(loggerFactory, _options);
             _client = new(_channel);
+        }
+
+        private static GrpcChannel CreateChannel(ILoggerFactory loggerFactory, IIdentityOptions options)
+        {
+            MethodConfig defaultMethodConfig = new()
+            {
+                Names = {MethodName.Default},
+                RetryPolicy = new RetryPolicy
+                {
+                    MaxAttempts = options.RetryTimes,
+                    InitialBackoff = options.InitialBackOff,
+                    MaxBackoff = options.MaxBackOff,
+                    BackoffMultiplier = options.BackOffMultiplier,
+                    RetryableStatusCodes = {StatusCode.Unavailable, StatusCode.DeadlineExceeded}
+                }
+            };
+            ServiceConfig serviceConfig = new()
+            {
+                MethodConfigs = {defaultMethodConfig}
+            };
+            SocketsHttpHandler httpHandler = new()
+            {
+                PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+                KeepAlivePingDelay = TimeSpan.FromSeconds(60),
+                KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
+                EnableMultipleHttp2Connections = true
+            };
+
+            return GrpcChannel.ForAddress(options.Address, new GrpcChannelOptions
+            {
+                LoggerFactory = loggerFactory,
+                ServiceConfig = serviceConfig,
+                HttpHandler = httpHandler
+            });
         }
 
         public void Dispose()
@@ -29,11 +80,25 @@ namespace CecoChat.Messaging.Server.Identity
             _channel.Dispose();
         }
 
-        public async Task<long> GenerateIdentity(long userID)
+        public async Task<GenerateIdentityResult> GenerateIdentity(long userID)
         {
             GenerateIdentityRequest request = new() {OriginatorId = userID};
-            GenerateIdentityResponse response = await _client.GenerateIdentityAsync(request);
-            return response.Id;
+            DateTime deadline = DateTime.UtcNow.Add(_options.CallTimeout);
+
+            try
+            {
+                GenerateIdentityResponse response = await _client.GenerateIdentityAsync(request, deadline: deadline);
+                return new GenerateIdentityResult
+                {
+                    Success = true,
+                    ID = response.Id
+                };
+            }
+            catch (RpcException rpcException)
+            {
+                _logger.LogError(rpcException, "Failed to generate ID for user {0}.", userID);
+                return new GenerateIdentityResult();
+            }
         }
     }
 }
--- source/CecoChat.Messaging.Server/Identity/IdentityOptions.cs
+++ source/CecoChat.Messaging.Server/Identity/IdentityOptions.cs
@@ -5,10 +5,30 @@ namespace CecoChat.Messaging.Server.Identity
     public interface IIdentityOptions
     {
         Uri Address { get; }
+
+        TimeSpan CallTimeout { get; }
+
+        int RetryTimes { get; }
+
+        TimeSpan InitialBackOff { get; }
+
+        int BackOffMultiplier {get;}
+
+        TimeSpan MaxBackOff { get; }
     }
 
     public sealed class IdentityOptions : IIdentityOptions
     {
         public Uri Address { get; set; }
+
+        public TimeSpan CallTimeout { get; set; }
+
+        public int RetryTimes { get; set; }
+
+        public TimeSpan InitialBackOff { get; set; }
+
+        public int BackOffMultiplier { get; set; }
+
+        public TimeSpan MaxBackOff { get; set; }
     }
 }
