d4c1d5a93914e51643fe7ac7922c033a7685b91c
ceco <cvetomir.todorov@gmail.com>
2021-04-27 13:27:32 +0300

    Add Dockerfile for Identity server

--- run/cecochat/BuildAllImages.ps1
+++ run/cecochat/BuildAllImages.ps1
@@ -1,5 +1,6 @@
 docker build -f .\Connect.Dockerfile -t ceco.com/cecochat/connect:0.1 ../../source/
 docker build -f .\History.Dockerfile -t ceco.com/cecochat/history:0.1 ../../source/
+docker build -f .\Identity.Dockerfile -t ceco.com/cecochat/identity:0.1 ../../source/
 docker build -f .\Materialize.Dockerfile -t ceco.com/cecochat/materialize:0.1 ../../source/
 docker build -f .\Messaging.Dockerfile -t ceco.com/cecochat/messaging:0.1 ../../source/
 docker build -f .\Profile.Dockerfile -t ceco.com/cecochat/profile:0.1 ../../source/
---
+++ run/cecochat/Identity.Dockerfile
@@ -0,0 +1,10 @@
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+WORKDIR /app
+
+COPY . ./
+RUN dotnet publish CecoChat.Identity.Server/CecoChat.Identity.Server.csproj -c Release -o out
+
+FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS runtime
+WORKDIR /app
+COPY --from=build /app/out .
+ENTRYPOINT ["dotnet", "CecoChat.Identity.Server.dll"]
