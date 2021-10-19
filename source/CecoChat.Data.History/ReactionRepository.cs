5a0ca2b4e18d8cbfc217673f6b4233f039f4ecda
ceco <cvetomir.todorov@gmail.com>
2021-10-20 00:54:37 +0300

    Add reactions to the messages_for_user table

--- source/CecoChat.Data.History/ReactionRepository.cs
+++ source/CecoChat.Data.History/ReactionRepository.cs
@@ -23,7 +23,9 @@ namespace CecoChat.Data.History
         private readonly IHistoryActivityUtility _historyActivityUtility;
         private readonly IDataUtility _dataUtility;
         private readonly Lazy<PreparedStatement> _setReactionQuery;
+        private readonly Lazy<PreparedStatement> _setReactionQueryForUser;
         private readonly Lazy<PreparedStatement> _unsetReactionQuery;
+        private readonly Lazy<PreparedStatement> _unsetReactionQueryForUser;
 
         public ReactionRepository(
             ILogger<ReactionRepository> logger,
@@ -35,19 +37,27 @@ namespace CecoChat.Data.History
             _dataUtility = dataUtility;
 
             _setReactionQuery = new Lazy<PreparedStatement>(() => _dataUtility.PrepareQuery(SetReactionCommand));
+            _setReactionQueryForUser = new Lazy<PreparedStatement>(() => _dataUtility.PrepareQuery(SetReactionCommandForUser));
             _unsetReactionQuery = new Lazy<PreparedStatement>(() => _dataUtility.PrepareQuery(UnsetReactionCommand));
+            _unsetReactionQueryForUser = new Lazy<PreparedStatement>(() => _dataUtility.PrepareQuery(UnsetReactionCommandForUser));
         }
 
         private const string SetReactionCommand =
             "UPDATE messages_for_dialog SET reactions[?] = ? WHERE dialog_id = ? AND message_id = ?";
+        private const string SetReactionCommandForUser =
+            "UPDATE messages_for_user SET reactions[?] = ? WHERE user_id = ? AND message_id = ?";
         private const string UnsetReactionCommand =
             "DELETE reactions[?] FROM messages_for_dialog WHERE dialog_id = ? AND message_id = ?";
+        private const string UnsetReactionCommandForUser =
+            "DELETE reactions[?] FROM messages_for_user WHERE user_id = ? AND message_id = ?";
 
         public void Prepare()
         {
             PreparedStatement _ = _setReactionQuery.Value;
             #pragma warning disable IDE0059
             PreparedStatement __ = _unsetReactionQuery.Value;
+            PreparedStatement ___ = _setReactionQueryForUser.Value;
+            PreparedStatement ____ = _unsetReactionQueryForUser.Value;
             #pragma warning restore IDE0059
         }
 
@@ -63,6 +73,12 @@ namespace CecoChat.Data.History
                 query.SetConsistencyLevel(ConsistencyLevel.LocalQuorum);
                 query.SetIdempotence(false);
                 await _dataUtility.MessagingSession.ExecuteAsync(query);
+
+                BoundStatement senderQuery = _setReactionQueryForUser.Value.Bind(message.ReactorId, message.Reaction, message.SenderId, message.MessageId);
+                await _dataUtility.MessagingSession.ExecuteAsync(senderQuery);
+                BoundStatement receiverQuery = _setReactionQueryForUser.Value.Bind(message.ReactorId, message.Reaction, message.ReceiverId, message.MessageId);
+                await _dataUtility.MessagingSession.ExecuteAsync(receiverQuery);
+
                 success = true;
                 _logger.LogTrace("User {0} reacted with {1} to message {2}.", message.ReactorId, message.Reaction, message.MessageId);
             }
@@ -84,6 +100,12 @@ namespace CecoChat.Data.History
                 query.SetConsistencyLevel(ConsistencyLevel.LocalQuorum);
                 query.SetIdempotence(false);
                 await _dataUtility.MessagingSession.ExecuteAsync(query);
+
+                BoundStatement senderQuery = _unsetReactionQueryForUser.Value.Bind(message.ReactorId, message.SenderId, message.MessageId);
+                await _dataUtility.MessagingSession.ExecuteAsync(senderQuery);
+                BoundStatement receiverQuery = _unsetReactionQueryForUser.Value.Bind(message.ReactorId, message.ReceiverId, message.MessageId);
+                await _dataUtility.MessagingSession.ExecuteAsync(receiverQuery);
+
                 success = true;
                 _logger.LogTrace("User {0} removed reaction to message {1}.", message.ReactorId, message.MessageId);
             }
