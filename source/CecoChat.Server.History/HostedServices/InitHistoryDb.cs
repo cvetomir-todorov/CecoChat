9df622275b86ab490d76271fee2bacc720a464b1
ceco <cvetomir.todorov@gmail.com>
2021-10-27 01:29:35 +0300

    Move query preparation into the DB hosted service

--- source/CecoChat.Server.History/HostedServices/InitHistoryDb.cs
+++ source/CecoChat.Server.History/HostedServices/InitHistoryDb.cs
@@ -1,24 +1,56 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using CecoChat.Cassandra;
 using CecoChat.Data.History;
+using CecoChat.Data.History.Repos;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace CecoChat.Server.History.HostedServices
 {
     public sealed class InitHistoryDb : IHostedService
     {
+        private readonly ILogger _logger;
         private readonly ICassandraDbInitializer _dbInitializer;
+        private readonly IHistoryRepo _historyRepo;
+        private readonly INewMessageRepo _newMessageRepo;
+        private readonly IReactionRepo _reactionRepo;
 
         public InitHistoryDb(
-            ICassandraDbInitializer dbInitializer)
+            ILogger<InitHistoryDb> logger,
+            ICassandraDbInitializer dbInitializer,
+            IHistoryRepo historyRepo,
+            INewMessageRepo newMessageRepo,
+            IReactionRepo reactionRepo)
         {
+            _logger = logger;
             _dbInitializer = dbInitializer;
+            _historyRepo = historyRepo;
+            _newMessageRepo = newMessageRepo;
+            _reactionRepo = reactionRepo;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _dbInitializer.Initialize(keyspace: "history", scriptSource: typeof(IHistoryDbContext).Assembly);
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    _logger.LogInformation("Start preparing queries...");
+                    _historyRepo.Prepare();
+                    _newMessageRepo.Prepare();
+                    _reactionRepo.Prepare();
+                    _logger.LogInformation("Completed preparing queries.");
+                }
+                catch (Exception exception)
+                {
+                    _logger.LogCritical(exception, "Failed to prepare queries.");
+                }
+            }, cancellationToken);
+
             return Task.CompletedTask;
         }
 
