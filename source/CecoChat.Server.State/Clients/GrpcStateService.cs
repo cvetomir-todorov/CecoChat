4fcf4943615a9b37cd581634d17abaa21a1a1685
ceco <cvetomir.todorov@gmail.com>
2021-11-11 01:27:06 +0200

    Remove send message operation from the state service

--- source/CecoChat.Server.State/Clients/GrpcStateService.cs
+++ source/CecoChat.Server.State/Clients/GrpcStateService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using CecoChat.Contracts.State;
-using CecoChat.Data;
 using CecoChat.Data.State.Repos;
 using CecoChat.Server.Identity;
 using Grpc.Core;
@@ -42,51 +41,6 @@ namespace CecoChat.Server.State.Clients
 
             _logger.LogTrace("Responding with {0} chats for user {1}.", chats.Count, userID);
             return response;
-        }
-
-        [Authorize(Roles = "user")]
-        public override Task<SendMessageResponse> SendMessage(SendMessageRequest request, ServerCallContext context)
-        {
-            long userID = GetUserID(context);
-            string chatID = DataUtility.CreateChatID(userID, request.ReceiverId);
-            UpdateSenderChat(userID, chatID, request.MessageId);
-            UpdateReceiverChat(request.ReceiverId, chatID, request.MessageId);
-
-            return Task.FromResult(new SendMessageResponse());
-        }
-
-        private void UpdateSenderChat(long senderID, string chatID, long messageID)
-        {
-            ChatState chat = GetChatFromDBIntoCache(senderID, chatID);
-            chat.NewestMessage = Math.Max(chat.NewestMessage, messageID);
-
-            _repo.UpdateChat(senderID, chat);
-        }
-
-        private void UpdateReceiverChat(long receiverID, string chatID, long messageID)
-        {
-            ChatState chat = GetChatFromDBIntoCache(receiverID, chatID);
-            chat.NewestMessage = Math.Max(chat.NewestMessage, messageID);
-            chat.OtherUserDelivered = Math.Max(chat.OtherUserDelivered, messageID);
-            chat.OtherUserSeen = Math.Max(chat.OtherUserSeen, messageID);
-
-            _repo.UpdateChat(receiverID, chat);
-        }
-
-        private ChatState GetChatFromDBIntoCache(long userID, string chatID)
-        {
-            if (!_cache.TryGetUserChat(userID, chatID, out ChatState chat))
-            {
-                chat = _repo.GetChat(userID, chatID);
-                chat ??= new ChatState
-                {
-                    ChatId = chatID
-                };
-
-                _cache.UpdateUserChat(userID, chat);
-            }
-
-            return chat;
         }
 
         private static long GetUserID(ServerCallContext context)
