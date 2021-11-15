0317ca0bce6271c74148a0176cd2cdd2db7b5b51
ceco <cvetomir.todorov@gmail.com>
2021-11-15 13:55:50 +0200

    Create the empty BFF project

---
+++ source/CecoChat.Server.Bff/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CecoChat.Server.Bff
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {}
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
