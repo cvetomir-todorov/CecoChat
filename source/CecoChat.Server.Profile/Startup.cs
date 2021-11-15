abc0f2d8f1cb198034208f5c46f15bc78c1e88d0
ceco <cvetomir.todorov@gmail.com>
2021-11-15 15:38:02 +0200

    Introduce tracing into BFF

--- source/CecoChat.Server.Profile/Startup.cs
+++ source/CecoChat.Server.Profile/Startup.cs
@@ -1,45 +1,14 @@
-using CecoChat.Otel;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry.Trace;
 
 namespace CecoChat.Server.Profile
 {
     public class Startup
     {
-        private readonly OtelSamplingOptions _otelSamplingOptions;
-        private readonly JaegerOptions _jaegerOptions;
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-
-            _jaegerOptions = new();
-            Configuration.GetSection("Jaeger").Bind(_jaegerOptions);
-
-            _otelSamplingOptions = new();
-            Configuration.GetSection("OtelSampling").Bind(_otelSamplingOptions);
-        }
-
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
-        {
-            // telemetry
-            services.AddOpenTelemetryTracing(otel =>
-            {
-                otel.AddServiceResource(new OtelServiceResource {Namespace = "CecoChat", Name = "Profile", Version = "0.1"});
-                otel.AddAspNetCoreInstrumentation(aspnet => aspnet.EnableGrpcAspNetCoreSupport = true);
-                otel.ConfigureSampling(_otelSamplingOptions);
-                otel.ConfigureJaegerExporter(_jaegerOptions);
-            });
-
-            // required
-            services.AddOptions();
-        }
+        {}
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
