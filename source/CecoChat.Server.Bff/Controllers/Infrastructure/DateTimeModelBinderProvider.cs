d5d5c4770bd99b089a77b3cec517d9bea98bbb47
ceco <cvetomir.todorov@gmail.com>
2021-11-17 20:56:39 +0200

    Fix date time format and UTC issues

---
+++ source/CecoChat.Server.Bff/Controllers/Infrastructure/DateTimeModelBinderProvider.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace CecoChat.Server.Bff.Controllers.Infrastructure
+{
+    public class DateTimeModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (context.Metadata.ModelType != typeof(DateTime) && context.Metadata.ModelType != typeof(DateTime?))
+            {
+                return null;
+            }
+
+            return new BinderTypeModelBinder(typeof(DateTimeModelBinder));
+        }
+    }
+}
