6da46e5338a59e01a02c29000b0f5c134b907354
ceco <cvetomir.todorov@gmail.com>
2021-11-22 19:26:39 +0200

    Add retry to history and state clients in BFF

--- source/CecoChat.Client.History/HistoryOptions.cs
+++ source/CecoChat.Client.History/HistoryOptions.cs
@@ -1,9 +1,12 @@
 using System;
+using CecoChat.Polly;
 
 namespace CecoChat.Client.History
 {
     public sealed class HistoryOptions
     {
         public Uri Address { get; set; }
+
+        public RetryOptions Retry { get; set; }
     }
 }
