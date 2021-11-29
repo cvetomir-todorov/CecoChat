166bc224b4cdabadf965e99b7a753a553d928593
ceco <cvetomir.todorov@gmail.com>
2021-11-29 16:15:28 +0200

    Update cecochat containerization

--- run/cecochat/build-all-images.sh
+++ run/cecochat/build-all-images.sh
@@ -1,5 +1,5 @@
-docker build -f Connect.Dockerfile -t ceco.com/cecochat/connect:0.1 ../../source/
+docker build -f Bff.Dockerfile -t ceco.com/cecochat/bff:0.1 ../../source/
+docker build -f Messaging.Dockerfile -t ceco.com/cecochat/messaging:0.1 ../../source/
+docker build -f State.Dockerfile -t ceco.com/cecochat/state:0.1 ../../source/
 docker build -f History.Dockerfile -t ceco.com/cecochat/history:0.1 ../../source/
 docker build -f IDGen.Dockerfile -t ceco.com/cecochat/idgen:0.1 ../../source/
-docker build -f Messaging.Dockerfile -t ceco.com/cecochat/messaging:0.1 ../../source/
-docker build -f Profile.Dockerfile -t ceco.com/cecochat/profile:0.1 ../../source/
