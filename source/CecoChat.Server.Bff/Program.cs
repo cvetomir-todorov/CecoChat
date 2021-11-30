a07c1a94b42ab0b513e008b584fea31468dc38a2
ceco <cvetomir.todorov@gmail.com>
2021-11-30 16:31:04 +0200

    Add missing static keyword

--- source/CecoChat.Server.Bff/Program.cs
+++ source/CecoChat.Server.Bff/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace CecoChat.Server.Bff
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
