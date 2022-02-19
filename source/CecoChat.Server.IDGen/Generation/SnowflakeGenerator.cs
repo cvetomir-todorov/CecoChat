630f301ff8ce66938126e9e1d221833a43c633e9
ceco <cvetomir.todorov@gmail.com>
2022-02-19 18:00:38 +0200

    Use snowflake generator IDs in IDGen from dynamic config

--- source/CecoChat.Server.IDGen/Generation/SnowflakeGenerator.cs
+++ source/CecoChat.Server.IDGen/Generation/SnowflakeGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CecoChat.Data.Config.Snowflake;
 using IdGen;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -22,12 +23,12 @@ namespace CecoChat.Server.IDGen.Generation
 
         public SnowflakeGenerator(
             ILogger<SnowflakeGenerator> logger,
-            IOptions<SnowflakeOptions> options,
+            IOptions<ConfigOptions> configOptions,
+            ISnowflakeConfig snowflakeConfig,
             INonCryptoHash hashFunction)
         {
             _logger = logger;
             _hashFunction = hashFunction;
-            SnowflakeOptions snowflakeOptions = options.Value;
 
             // IdGen doesn't use the sign bit so the sum of bits is 63
             IdStructure idStructure = new(timestampBits: 41, generatorIdBits: 8, sequenceBits: 14);
@@ -35,7 +36,8 @@ namespace CecoChat.Server.IDGen.Generation
             IdGeneratorOptions idGenOptions = new(idStructure, timeSource, SequenceOverflowStrategy.SpinWait);
 
             _generators = new List<IdGenerator>();
-            foreach (short generatorID in snowflakeOptions.GeneratorIDs)
+            IReadOnlyCollection<short> generatorIDs = snowflakeConfig.GetGeneratorIDs(configOptions.Value.ServerID);
+            foreach (short generatorID in generatorIDs)
             {
                 _generators.Add(new IdGenerator(generatorID, idGenOptions));
             }
@@ -57,8 +59,16 @@ namespace CecoChat.Server.IDGen.Generation
 
         private IdGenerator ChooseGenerator(long originatorID, out int generatorIndex)
         {
-            int hash = _hashFunction.Compute(originatorID);
-            generatorIndex = Math.Abs(hash) % _generators.Count;
+            if (_generators.Count == 1)
+            {
+                generatorIndex = 0;
+            }
+            else
+            {
+                int hash = _hashFunction.Compute(originatorID);
+                generatorIndex = Math.Abs(hash) % _generators.Count;
+            }
+
             return _generators[generatorIndex];
         }
     }
--- source/CecoChat.Server.IDGen/Generation/SnowflakeOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace CecoChat.Server.IDGen.Generation
-{
-    public sealed class SnowflakeOptions
-    {
-        public List<short> GeneratorIDs { get; set; }
-    }
-}
