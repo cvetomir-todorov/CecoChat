3c21fa3ef338c50ce563ceb032e4a033c772a6e2
ceco <cvetomir.todorov@gmail.com>
2022-02-21 14:03:58 +0200

    Show snowflake total server count and current server ID

--- source/CecoChat.Server.IDGen/HostedServices/InitDynamicConfig.cs
+++ source/CecoChat.Server.IDGen/HostedServices/InitDynamicConfig.cs
@@ -1,21 +1,32 @@
 using System.Threading;
 using System.Threading.Tasks;
 using CecoChat.Data.Config.Snowflake;
+using CecoChat.Server.IDGen.Generation;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace CecoChat.Server.IDGen.HostedServices
 {
     public sealed class InitDynamicConfig : IHostedService
     {
+        private readonly ILogger _logger;
+        private readonly ConfigOptions _configOptions;
         private readonly ISnowflakeConfig _snowflakeConfig;
 
-        public InitDynamicConfig(ISnowflakeConfig snowflakeConfig)
+        public InitDynamicConfig(
+            ILogger<InitDynamicConfig> logger,
+            IOptions<ConfigOptions> configOptions,
+            ISnowflakeConfig snowflakeConfig)
         {
+            _logger = logger;
+            _configOptions = configOptions.Value;
             _snowflakeConfig = snowflakeConfig;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Configured server ID is '{0}'.", _configOptions.ServerID);
             await _snowflakeConfig.Initialize();
         }
 
