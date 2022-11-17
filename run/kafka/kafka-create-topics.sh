e10246ea6a20616b97bfad38275f4e240e8d905f
ceco <cvetomir.todorov@protonmail.com>
2022-11-17 18:38:00 +0200

    Update script for creating kafka backplane topics

--- run/kafka/kafka-create-topics.sh
+++ run/kafka/kafka-create-topics.sh
@@ -1,2 +1,1 @@
-kafka-topics --zookeeper kafka1:9091 --create --topic messages-by-receiver --partitions 12 --replication-factor 2 --config min.insync.replicas=2
-kafka-topics --zookeeper kafka1:9091 --create --topic messages-by-sender --partitions 12 --replication-factor 2 --config min.insync.replicas=2
+kafka-topics --bootstrap-server kafka1:9091 --create --topic messages-by-receiver --partitions 12 --replication-factor 2 --config min.insync.replicas=2
