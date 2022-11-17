6f95e9b62ec97713cb44bc5b982270035830d223
ceco <cvetomir.todorov@protonmail.com>
2022-11-17 18:28:37 +0200

    Stop replicating messages from topic 'messages-by-receiver' into 'messages-by-sender'

--- source/CecoChat.Server.Messaging/Backplane/BackplaneComponents.cs
+++ source/CecoChat.Server.Messaging/Backplane/BackplaneComponents.cs
@@ -17,29 +17,25 @@ public sealed class BackplaneComponents : IBackplaneComponents
     private readonly ITopicPartitionFlyweight _topicPartitionFlyweight;
     private readonly ISendersProducer _sendersProducer;
     private readonly IReceiversConsumer _receiversConsumer;
-    private readonly IMessageReplicator _messageReplicator;
 
     public BackplaneComponents(
         ILogger<BackplaneComponents> logger,
         IOptions<BackplaneOptions> backplaneOptions,
         ITopicPartitionFlyweight topicPartitionFlyweight,
         ISendersProducer sendersProducer,
-        IReceiversConsumer receiversConsumer,
-        IMessageReplicator messageReplicator)
+        IReceiversConsumer receiversConsumer)
     {
         _logger = logger;
         _backplaneOptions = backplaneOptions.Value;
         _topicPartitionFlyweight = topicPartitionFlyweight;
         _sendersProducer = sendersProducer;
         _receiversConsumer = receiversConsumer;
-        _messageReplicator = messageReplicator;
     }
 
     public void Dispose()
     {
         _sendersProducer.Dispose();
         _receiversConsumer.Dispose();
-        _messageReplicator.Dispose();
     }
 
     public void ConfigurePartitioning(int partitionCount, PartitionRange partitions)
@@ -52,21 +48,11 @@ public sealed class BackplaneComponents : IBackplaneComponents
                 _backplaneOptions.TopicMessagesByReceiver, currentPartitionCount, partitionCount);
         }
 
-        currentPartitionCount = _topicPartitionFlyweight.GetTopicPartitionCount(_backplaneOptions.TopicMessagesBySender);
-        if (currentPartitionCount < partitionCount)
-        {
-            _topicPartitionFlyweight.AddOrUpdate(_backplaneOptions.TopicMessagesBySender, partitionCount);
-            _logger.LogInformation("Increase cached partitions for topic {Topic} from {CurrentPartitionCount} to {NewPartitionCount}",
-                _backplaneOptions.TopicMessagesBySender, currentPartitionCount, partitionCount);
-        }
-
         _sendersProducer.PartitionCount = partitionCount;
         _receiversConsumer.Prepare(partitions);
-        _messageReplicator.PartitionCount = partitionCount;
-        _messageReplicator.Prepare(partitions);
 
-        _logger.LogInformation("Prepared backplane components for topics {TopicMessagesByReceiver} and {TopicMessagesBySender} to use partitions {Partitions}",
-            _backplaneOptions.TopicMessagesByReceiver, _backplaneOptions.TopicMessagesBySender, partitions);
+        _logger.LogInformation("Prepared backplane components for topic {TopicMessagesByReceiver} to use partitions {Partitions}",
+            _backplaneOptions.TopicMessagesByReceiver, partitions);
     }
 
     public void StartConsumption(CancellationToken ct)
@@ -80,18 +66,6 @@ public sealed class BackplaneComponents : IBackplaneComponents
             catch (Exception exception)
             {
                 _logger.LogCritical(exception, "Failure in consumer {ConsumerId}", _receiversConsumer.ConsumerID);
-            }
-        }, ct, TaskCreationOptions.LongRunning, TaskScheduler.Current);
-
-        Task.Factory.StartNew(() =>
-        {
-            try
-            {
-                _messageReplicator.Start(ct);
-            }
-            catch (Exception exception)
-            {
-                _logger.LogCritical(exception, "Failure in consumer {ConsumerId}", _messageReplicator.ConsumerID);
             }
         }, ct, TaskCreationOptions.LongRunning, TaskScheduler.Current);
     }
