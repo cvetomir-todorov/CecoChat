3fe0d6ae298948721b5841112bd94b856ee8a5ee
ceco <cvetomir.todorov@protonmail.com>
2022-12-01 14:36:47 +0200

    Add a reusable status health check

---
+++ source/CecoChat/Health/StatusHealthCheck.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CecoChat.Health;
+
+public class StatusHealthCheck : IHealthCheck
+{
+    private volatile bool _isReady;
+
+    public bool IsReady
+    {
+        get => _isReady;
+        set => _isReady = value;
+    }
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        if (IsReady)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+        else
+        {
+            return Task.FromResult(HealthCheckResult.Unhealthy());
+        }
+    }
+}
