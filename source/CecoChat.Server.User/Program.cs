e87ab9214c3aa9b28da5599fb0e9c8812ec0d532
ceco <cvetomir.todorov@protonmail.com>
2023-02-08 17:57:32 +0200

    Add user service with endpoints for accessing profiles

---
+++ source/CecoChat.Server.User/Program.cs
@@ -0,0 +1,10 @@
+namespace CecoChat.Server.User;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        IHostBuilder hostBuilder = EntryPoint.CreateDefaultHostBuilder(args, startupContext: typeof(Startup));
+        EntryPoint.CreateAndRunHost(hostBuilder, loggerContext: typeof(Program));
+    }
+}
