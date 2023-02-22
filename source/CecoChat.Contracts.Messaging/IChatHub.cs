ed0f8a2912f73c829f8eef233d1259339b0809a7
ceco <cvetomir.todorov@protonmail.com>
2023-02-22 21:43:23 +0200

    Change Messaging contracts to service interfaces and C# POCOs

---
+++ source/CecoChat.Contracts.Messaging/IChatHub.cs
@@ -0,0 +1,48 @@
+namespace CecoChat.Contracts.Messaging;
+
+public interface IChatHub
+{
+    Task Listen(ListenSubscription subscription);
+
+    Task<SendMessageResponse> SendMessage(SendMessageRequest request);
+
+    Task<ReactResponse> React(ReactRequest request);
+
+    Task<UnReactResponse> UnReact(UnReactRequest request);
+}
+
+public sealed class ListenSubscription
+{ }
+
+public sealed class SendMessageRequest
+{
+    public long ReceiverId { get; set; }
+    public DataType DataType { get; set; }
+    public string Data { get; set; } = string.Empty;
+}
+
+public sealed class SendMessageResponse
+{
+    public long MessageId { get; set; }
+}
+
+public sealed class ReactRequest
+{
+    public long MessageId { get; set; }
+    public long SenderId { get; set; }
+    public long ReceiverId { get; set; }
+    public string Reaction { get; set; } = string.Empty;
+}
+
+public sealed class ReactResponse
+{ }
+
+public sealed class UnReactRequest
+{
+    public long MessageId { get; set; }
+    public long SenderId { get; set; }
+    public long ReceiverId { get; set; }
+}
+
+public sealed class UnReactResponse
+{ }
