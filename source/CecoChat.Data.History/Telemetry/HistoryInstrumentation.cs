c585fcac94d25367124166bc7886754c1c58733c
ceco <cvetomir.todorov@protonmail.com>
2023-02-24 17:29:12 +0200

    Standardize OpenTelemetry span names for Kafka and Cassandra StateDB/HistoryDB

--- source/CecoChat.Data.History/Telemetry/HistoryInstrumentation.cs
+++ source/CecoChat.Data.History/Telemetry/HistoryInstrumentation.cs
@@ -13,9 +13,9 @@ internal static class HistoryInstrumentation
 
     public static class Operations
     {
-        public const string AddDataMessage = "HistoryDB.AddDataMessage";
-        public const string GetHistory = "HistoryDB.GetHistory";
-        public const string SetReaction = "HistoryDB.SetReaction";
-        public const string UnsetReaction = "HistoryDB.UnsetReaction";
+        public const string AddDataMessage = "HistoryDB/AddDataMessage";
+        public const string GetHistory = "HistoryDB/GetHistory";
+        public const string SetReaction = "HistoryDB/SetReaction";
+        public const string UnsetReaction = "HistoryDB/UnsetReaction";
     }
 }
