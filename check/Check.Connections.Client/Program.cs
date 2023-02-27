d8c50ef8ad8cb00a94bb0d1ebc7e523fe45e519c
ceco <cvetomir.todorov@protonmail.com>
2023-02-27 19:16:34 +0200

    Change code for checking connection limits to use websocket

--- check/Check.Connections.Client/Program.cs
+++ check/Check.Connections.Client/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using CecoChat.Contracts.Messaging;
 using CommandLine;
-using Grpc.Net.Client;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Check.Connections.Client;
 
@@ -25,20 +24,19 @@ public static class Program
     private static void Start(CommandLine commandLine)
     {
         List<ClientData> clients = CreateClients(commandLine);
-        Console.WriteLine("Create {0} clients with {1} messages each and server {2}.",
-            commandLine.ClientCount, commandLine.MessageCount, commandLine.ServerAddress);
+        Console.WriteLine("Created {0} clients with {1} messages each and server {2}.", clients.Count, commandLine.MessageCount, commandLine.ServerAddress);
 
+        Console.WriteLine("Start connecting clients...");
         Stopwatch stopwatch = Stopwatch.StartNew();
         ConnectClients(clients);
         stopwatch.Stop();
-        Console.WriteLine("Clients connected for {0:0.####} ms.", stopwatch.Elapsed.TotalMilliseconds);
+        Console.WriteLine("{0} client(s) connected to server {1} for {2:0.####}s.", clients.Count, commandLine.ServerAddress, stopwatch.Elapsed.TotalSeconds);
 
+        Console.WriteLine("Start sending messages...");
         stopwatch.Restart();
-        Console.WriteLine("Start sending messages.");
         RunAllClients(clients, commandLine);
         stopwatch.Stop();
-        Console.WriteLine("Completed {0} out of {1} clients for {2:0.####} ms.",
-            _successCount, commandLine.ClientCount, stopwatch.Elapsed.TotalMilliseconds);
+        Console.WriteLine("Completed {0} out of {1} clients for {2:0.####}s.", _successCount, commandLine.ClientCount, stopwatch.Elapsed.TotalSeconds);
     }
 
     private static List<ClientData> CreateClients(CommandLine commandLine)
@@ -47,26 +45,18 @@ public static class Program
 
         for (int i = 0; i < commandLine.ClientCount; ++i)
         {
-            Uri address = new(commandLine.ServerAddress);
-            EndPoint endPoint = new DnsEndPoint(address.Host, address.Port);
-            PreConnectedHttpHandler preConnectedHttpHandler = new(endPoint);
-            SocketsHttpHandler socketsHttpHandler = new()
-            {
-                ConnectCallback = preConnectedHttpHandler.GetConnectedStream
-            };
+            UriBuilder addressBuilder = new(commandLine.ServerAddress);
+            addressBuilder.Path = "/chat";
 
-            GrpcChannel channel = GrpcChannel.ForAddress(commandLine.ServerAddress, new GrpcChannelOptions
-            {
-                HttpHandler = socketsHttpHandler
-            });
-            Send.SendClient sendClient = new(channel);
+            HubConnection connection = new HubConnectionBuilder()
+                .WithUrl(addressBuilder.Uri)
+                .AddMessagePackProtocol()
+                .Build();
 
             ClientData client = new()
             {
-                ID = i,
-                HttpHandler = preConnectedHttpHandler,
-                Channel = channel,
-                SendClient = sendClient
+                Id = i,
+                Connection = connection
             };
 
             clients.Add(client);
@@ -81,7 +71,7 @@ public static class Program
 
         for (int i = 0; i < clients.Count; ++i)
         {
-            connectTasks[i] = clients[i].HttpHandler.PreConnect();
+            connectTasks[i] = clients[i].Connection.StartAsync();
         }
 
         Task.WaitAll(connectTasks);
@@ -111,24 +101,25 @@ public static class Program
                     Data = "dummy"
                 };
 
-                await client.SendClient.SendMessageAsync(request);
+                await client.Connection.InvokeAsync(nameof(IChatHub.SendMessage), request);
                 await Task.Delay(TimeSpan.FromSeconds(1));
             }
 
             Interlocked.Increment(ref _successCount);
-            await client.Channel.ShutdownAsync();
         }
         catch (Exception exception)
         {
-            Console.WriteLine("Client {0} resulted in error: {1}", client.ID, exception.Message);
+            Console.WriteLine("Client {0} resulted in error: {1}", client.Id, exception.Message);
+        }
+        finally
+        {
+            await client.Connection.DisposeAsync();
         }
     }
 
     private sealed class ClientData
     {
-        public int ID { get; init; }
-        public PreConnectedHttpHandler HttpHandler { get; init; }
-        public GrpcChannel Channel { get; init; }
-        public Send.SendClient SendClient { get; init; }
+        public int Id { get; init; }
+        public HubConnection Connection { get; init; }
     }
 }
