73ed72c54e3f9ff54812e6e8e41de72e73fc1e03
ceco <cvetomir.todorov@protonmail.com>
2023-03-20 18:11:47 +0200

    Add k8s config, statefulset, service for backplane kafka along with script for creating topics

---
+++ deploy/minikube/backplane/kafka/topics.sh
@@ -0,0 +1,3 @@
+kafka-topics --bootstrap-server backplane-kafka-0.backplane-kafka.cecochat.svc.cluster.local:9092 --create --topic messages-by-receiver --partitions 12 --replication-factor 2 --config min.insync.replicas=2
+kafka-topics --bootstrap-server backplane-kafka-0.backplane-kafka.cecochat.svc.cluster.local:9092 --create --topic health --partitions 1 --replication-factor 1
+kafka-topics --bootstrap-server backplane-kafka-0.backplane-kafka.cecochat.svc.cluster.local:9092 --list
