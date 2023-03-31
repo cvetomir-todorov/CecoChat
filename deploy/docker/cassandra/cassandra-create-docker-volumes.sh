edd8ad373b70ba6626eda1354bb1a7b558b02012
ceco <cvetomir.todorov@protonmail.com>
2023-03-31 15:26:58 +0300

    Consolidate cassandra docker deployment

--- deploy/docker/cassandra/cassandra-create-docker-volumes.sh
+++
@@ -1,4 +0,0 @@
-docker volume create cecochat-cass1-data
-docker volume create cecochat-cass1-conf
-docker volume create cecochat-cass2-data
-docker volume create cecochat-cass2-conf
---
+++ deploy/docker/cassandra/docker-volumes.sh
@@ -0,0 +1,4 @@
+docker volume create cecochat-cassandra0-data
+docker volume create cecochat-cassandra0-conf
+docker volume create cecochat-cassandra1-data
+docker volume create cecochat-cassandra1-conf
