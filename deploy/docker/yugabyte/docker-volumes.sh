54bbdd615b093abbd95ba981af22c57d433f5a12
ceco <cvetomir.todorov@protonmail.com>
2023-03-31 18:09:04 +0300

    Consolidate yugabyte docker deployment

---
+++ deploy/docker/yugabyte/docker-volumes.sh
@@ -0,0 +1,3 @@
+docker volume create cecochat-yb-master
+docker volume create cecochat-yb-tserver0
+docker volume create cecochat-yb-tserver1
--- deploy/docker/yugabyte/yugabyte-create-docker-volumes.sh
+++
@@ -1,2 +0,0 @@
-docker volume create cecochat-yb-master
-docker volume create cecochat-yb-tserver
