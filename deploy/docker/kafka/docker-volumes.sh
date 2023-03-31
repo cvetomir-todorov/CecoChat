0782335d8c8741974b6b1a943618388c675f191e
ceco <cvetomir.todorov@protonmail.com>
2023-03-31 15:27:44 +0300

    Consolidate kafka docker deployment

--- deploy/docker/kafka/docker-volumes.sh
+++ deploy/docker/kafka/docker-volumes.sh
@@ -1,4 +1,4 @@
 docker volume create cecochat-zookeeper-data
 docker volume create cecochat-zookeeper-datalog
+docker volume create cecochat-kafka0-data
 docker volume create cecochat-kafka1-data
-docker volume create cecochat-kafka2-data
