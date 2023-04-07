9680beed7c2589335e21ff1f22b804c42549ee65
ceco <cvetomir.todorov@protonmail.com>
2023-04-07 11:34:58 +0300

    Add generating either console client or load testing users in the UserDB

--- source/CecoChat.Server.User/HostedServices/UserDbOptions.cs
+++ source/CecoChat.Server.User/HostedServices/UserDbOptions.cs
@@ -6,5 +6,8 @@ public sealed class UserDbOptions
 {
     public NpgsqlOptions Init { get; set; } = new();
     public bool Seed { get; set; }
+    public bool SeedConsoleClientUsers { get; set; }
+    public bool SeedLoadTestingUsers { get; set; }
+    public int SeedLoadTestingUserCount { get; set; }
     public NpgsqlOptions Connect { get; set; } = new();
 }
