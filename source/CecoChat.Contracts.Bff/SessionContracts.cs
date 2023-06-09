3103b876292389d295b459249b8da4d3658cc585
ceco <cvetomir.todorov@protonmail.com>
2023-06-09 14:05:09 +0300

    Consolidate JSON parameter names used in serialization and by Refit HTTP client generation

--- source/CecoChat.Contracts.Bff/SessionContracts.cs
+++ source/CecoChat.Contracts.Bff/SessionContracts.cs
@@ -1,27 +1,34 @@
+using System.Text.Json.Serialization;
 using Refit;
 
 namespace CecoChat.Contracts.Bff;
 
 public sealed class CreateSessionRequest
 {
+    [JsonPropertyName("username")]
     [AliasAs("username")]
     public string Username { get; init; } = string.Empty;
 
+    [JsonPropertyName("password")]
     [AliasAs("password")]
     public string Password { get; init; } = string.Empty;
 }
 
 public sealed class CreateSessionResponse
 {
-    [AliasAs("clientID")]
+    [JsonPropertyName("clientId")]
+    [AliasAs("clientId")]
     public Guid ClientId { get; init; }
 
+    [JsonPropertyName("accessToken")]
     [AliasAs("accessToken")]
     public string AccessToken { get; init; } = string.Empty;
 
+    [JsonPropertyName("profile")]
     [AliasAs("profile")]
     public ProfileFull Profile { get; init; } = null!;
 
+    [JsonPropertyName("messagingServerAddress")]
     [AliasAs("messagingServerAddress")]
     public string MessagingServerAddress { get; init; } = string.Empty;
 }
