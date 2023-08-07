53784ed3cea05d9c13ce7527104d6f59f708bf66
ceco <cvetomir.todorov@protonmail.com>
2023-08-07 22:23:59 +0300

    Add change password and update profile to BFF service and the console client

---
+++ source/CecoChat.ConsoleClient/Interaction/EditProfileState.cs
@@ -0,0 +1,52 @@
+using CecoChat.ConsoleClient.Api;
+
+namespace CecoChat.ConsoleClient.Interaction;
+
+public class EditProfileState : State
+{
+    public EditProfileState(StateContainer states) : base(states)
+    { }
+
+    public override async Task<State> Execute()
+    {
+        Console.Clear();
+
+        if (Client.UserProfile == null)
+        {
+            throw new InvalidOperationException("Client is not connected.");
+        }
+        
+        DisplayUserData();
+        Console.WriteLine("Edit your profile - press empty without entering anything to preserve current.");
+
+        Console.Write("Enter display name, current is '{0}': ", Client.UserProfile.DisplayName);
+        string displayName = Console.ReadLine() ?? string.Empty;
+        if (displayName.Length == 0)
+        {
+            displayName = Client.UserProfile.DisplayName;
+        }
+
+        ClientResponse response = await Client.EditProfile(displayName);
+        if (!response.Success)
+        {
+            foreach (string error in response.Errors)
+            {
+                Console.WriteLine(error);
+            }
+
+            Console.WriteLine("If the error persists, try logging again. Press ENTER to return.");
+            Console.ReadLine();
+        }
+        else
+        {
+            Client.UserProfile.DisplayName = displayName;
+
+            Console.WriteLine("Profile edited successfully!");
+            Console.WriteLine("Press ENTER to return.");
+            Console.ReadLine();
+        }
+
+        Context.ReloadData = true;
+        return States.AllChats;
+    }
+}
--- source/CecoChat.Server.Bff/Infra/AutoMapperProfile.cs
+++ source/CecoChat.Server.Bff/Infra/AutoMapperProfile.cs
@@ -1,3 +1,5 @@
+using CecoChat.Contracts;
+
 namespace CecoChat.Server.Bff.Infra;
 
 public class AutoMapperProfile : AutoMapper.Profile
@@ -5,8 +7,17 @@ public class AutoMapperProfile : AutoMapper.Profile
     public AutoMapperProfile()
     {
         CreateMap<Contracts.Bff.RegisterRequest, Contracts.User.ProfileCreate>();
-
-        CreateMap<Contracts.User.ProfileFull, Contracts.Bff.ProfileFull>();
+        CreateMap<Contracts.Bff.ChangePasswordRequest, Contracts.User.ProfileChangePassword>()
+            .ForMember(
+                profileContract => profileContract.Version,
+                options => options.MapFrom(request => request.Version.ToUuid()));
+        CreateMap<Contracts.Bff.EditProfileRequest, Contracts.User.ProfileUpdate>()
+            .ForMember(profileContract => profileContract.Version,
+                options => options.MapFrom(request => request.Version.ToUuid()));
+        CreateMap<Contracts.User.ProfileFull, Contracts.Bff.ProfileFull>()
+            .ForMember(
+                profileBff => profileBff.Version,
+                options => options.MapFrom(profileContract => profileContract.Version.ToGuid()));
         CreateMap<Contracts.User.ProfilePublic, Contracts.Bff.ProfilePublic>();
     }
 }
