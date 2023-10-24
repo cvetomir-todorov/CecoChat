33f655ae1ce1f060bd2a472b61bfaa5ad7d7efd8
ceco <cvetomir.todorov@protonmail.com>
2023-10-24 17:04:29 +0300

    Add config DB to the user service

--- source/CecoChat.Server.User/HealthChecks.cs
+++ source/CecoChat.Server.User/HealthChecks.cs
@@ -2,5 +2,8 @@ using CecoChat.Health;
 
 namespace CecoChat.Server.User;
 
+public class ConfigDbInitHealthCheck : StatusHealthCheck
+{ }
+
 public class UserDbInitHealthCheck : StatusHealthCheck
 { }
