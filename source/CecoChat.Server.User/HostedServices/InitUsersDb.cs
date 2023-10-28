245798a8b534aa62893b83860a3837f104064301
ceco <cvetomir.todorov@protonmail.com>
2023-10-28 08:30:41 +0300

    Fix seeding User DB

--- source/CecoChat.Server.User/HostedServices/InitUsersDb.cs
+++ source/CecoChat.Server.User/HostedServices/InitUsersDb.cs
@@ -1,5 +1,6 @@
 using CecoChat.Data.User;
 using CecoChat.Npgsql;
+using CecoChat.Server.User.Security;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Npgsql;
@@ -37,8 +38,9 @@ public sealed class InitUsersDb : IHostedService
         {
             _logger.LogInformation("Seeding the database...");
 
-            await DeleteAllProfiles(cancellationToken);
-            _logger.LogInformation("Deleted all users");
+            int connectionCount = await DeleteAllConnections(cancellationToken);
+            int profileCount = await DeleteAllProfiles(cancellationToken);
+            _logger.LogInformation("Deleted {ProfileCount} profiles and {ConnectionCount} connections", profileCount, connectionCount);
 
             if (_options.SeedConsoleClientUsers)
             {
@@ -57,47 +59,47 @@ public sealed class InitUsersDb : IHostedService
 
     private async Task SeedConsoleClientUsers(CancellationToken ct)
     {
-        await DeleteAllProfiles(ct);
-
         ProfileEntity[] profiles =
         {
             new()
             {
                 UserId = 1,
-                UserName = "bob1",
+                UserName = "bobby",
                 DisplayName = "Robert",
-                AvatarUrl = "https://cdn.cecochat.com/avatars/bob1.jpg",
-                Email = "bob1@cecochat.com",
                 Phone = "+359888111111"
             },
             new()
             {
                 UserId = 2,
-                UserName = "alice2",
+                UserName = "alice",
                 DisplayName = "Alice in Wonderland",
-                AvatarUrl = "https://cdn.cecochat.com/avatars/alice2.jpg",
-                Email = "alice2@cecochat.com",
                 Phone = "+359888222222"
             },
             new()
             {
                 UserId = 3,
-                UserName = "john3",
+                UserName = "john",
                 DisplayName = "Sir John",
-                AvatarUrl = "https://cdn.cecochat.com/avatars/john3.jpg",
-                Email = "john3@cecochat.com",
                 Phone = "+359888333333"
             },
             new()
             {
                 UserId = 1200,
-                UserName = "peter1200",
+                UserName = "peter",
                 DisplayName = "Peter the Great",
-                AvatarUrl = "https://cdn.cecochat.com/avatars/peter1200.jpg",
-                Email = "peter1200@cecochat.com",
                 Phone = "+359888120012"
             }
         };
+
+        PasswordHasher passwordHasher = new();
+
+        foreach (ProfileEntity profile in profiles)
+        {
+            profile.Version = DateTime.UtcNow;
+            profile.Password = passwordHasher.Hash("secret12");
+            profile.AvatarUrl = $"https://cdn.cecochat.com/avatars/{profile.UserName}.jpg";
+            profile.Email = $"{profile.UserName}@cecochat.com";
+        }
 
         _dbContext.Profiles.AddRange(profiles);
         await _dbContext.SaveChangesAsync(ct);
@@ -106,8 +108,6 @@ public sealed class InitUsersDb : IHostedService
 
     private async Task SeedLoadTestingUsers(int userCount, CancellationToken ct)
     {
-        await DeleteAllProfiles(ct);
-
         ProfileEntity[] profiles = new ProfileEntity[userCount];
         for (int i = 0; i < userCount; ++i)
         {
@@ -128,9 +128,14 @@ public sealed class InitUsersDb : IHostedService
         _dbContext.ChangeTracker.Clear();
     }
 
+    private Task<int> DeleteAllConnections(CancellationToken ct)
+    {
+        return _dbContext.Database.ExecuteSqlRawAsync("DELETE from public.\"Connections\"", ct);
+    }
+
     private Task<int> DeleteAllProfiles(CancellationToken ct)
     {
-        return _dbContext.Database.ExecuteSqlRawAsync("TRUNCATE TABLE public.\"Profiles\"", ct);
+        return _dbContext.Database.ExecuteSqlRawAsync("DELETE from public.\"Profiles\"", ct);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
