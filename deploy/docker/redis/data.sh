66568cea727059c449c72879e73286acd838d3fd
ceco <cvetomir.todorov@protonmail.com>
2023-12-05 18:13:12 +0200

    Update dev service addresses

--- deploy/docker/redis/data.sh
+++ deploy/docker/redis/data.sh
@@ -1,7 +1,7 @@
 # partitioning
 redis-cli SET   partitioning.count 12
 redis-cli HMSET partitioning.partitions 0 "0-5" 1 "6-11"
-redis-cli HMSET partitioning.addresses 0 "https://localhost:31001" 1 "https://localhost:31011"
+redis-cli HMSET partitioning.addresses 0 "https://localhost:31000" 1 "https://localhost:31001"
 
 # history
 redis-cli SET history.message-count 32
