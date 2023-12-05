306bc2f742561523ec71555e7fcf90e8a7d86871
ceco <cvetomir.todorov@protonmail.com>
2023-12-05 18:24:21 +0200

    Merge state and history dockerfiles

--- package/cecochat/build-all-images.sh
+++ package/cecochat/build-all-images.sh
@@ -1,6 +1,5 @@
 docker build -f bff.dockerfile -t ceco.com/cecochat/bff:0.1 ../../source/
 docker build -f messaging.dockerfile -t ceco.com/cecochat/messaging:0.1 ../../source/
-docker build -f state.dockerfile -t ceco.com/cecochat/state:0.1 ../../source/
-docker build -f history.dockerfile -t ceco.com/cecochat/history:0.1 ../../source/
+docker build -f chats.dockerfile -t ceco.com/cecochat/chats:0.1 ../../source/
 docker build -f idgen.dockerfile -t ceco.com/cecochat/idgen:0.1 ../../source/
 docker build -f user.dockerfile -t ceco.com/cecochat/user:0.1 ../../source/
