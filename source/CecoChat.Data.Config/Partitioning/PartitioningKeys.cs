483db2c05ca58da5858c7f13aa83e94b5b5358f0
ceco <cvetomir.todorov@protonmail.com>
2023-12-08 12:45:48 +0200

    Move config keys into a common publicly accessible static class

--- source/CecoChat.Data.Config/Partitioning/PartitioningKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CecoChat.Data.Config.Partitioning;
-
-internal static class PartitioningKeys
-{
-    public const string Section = "partitioning";
-    public const string PartitionCount = "partitioning.count";
-    public const string Partitions = "partitioning.partitions";
-    public const string Addresses = "partitioning.addresses";
-}
