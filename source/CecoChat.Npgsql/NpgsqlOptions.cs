54967e91e6adbfd56865910f5815114322b6708e
ceco <cvetomir.todorov@protonmail.com>
2023-12-08 12:33:46 +0200

    Move Postgres settings for retry on failure in appsettings.json

--- source/CecoChat.Npgsql/NpgsqlOptions.cs
+++ source/CecoChat.Npgsql/NpgsqlOptions.cs
@@ -3,6 +3,12 @@ namespace CecoChat.Npgsql;
 public class NpgsqlOptions
 {
     public string ConnectionString { get; init; } = string.Empty;
+    
+    public bool EnableRetryOnFailure { get; init; }
+
+    public int MaxRetryCount { get; init; }
+
+    public TimeSpan MaxRetryDelay { get; init; }
 
     public int DbContextPoolSize { get; init; }
 
