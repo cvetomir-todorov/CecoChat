605c1928cd9a53003c199fa6d894a67325f07118
ceco <cvetomir.todorov@protonmail.com>
2023-12-13 17:04:40 +0200

    Add backplane into Admin service

--- source/CecoChat.Server.Admin/Startup.cs
+++ source/CecoChat.Server.Admin/Startup.cs
@@ -5,9 +5,13 @@ using CecoChat.AspNet.ModelBinding;
 using CecoChat.AspNet.Swagger;
 using CecoChat.Autofac;
 using CecoChat.Data.Config;
+using CecoChat.Kafka;
+using CecoChat.Kafka.Health;
 using CecoChat.Npgsql;
 using CecoChat.Npgsql.Health;
+using CecoChat.Server.Admin.Backplane;
 using CecoChat.Server.Admin.HostedServices;
+using CecoChat.Server.Backplane;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -16,10 +20,14 @@ namespace CecoChat.Server.Admin;
 
 public class Startup : StartupBase
 {
+    private readonly BackplaneOptions _backplaneOptions;
     private readonly SwaggerOptions _swaggerOptions;
 
     public Startup(IConfiguration configuration) : base(configuration)
     {
+        _backplaneOptions = new();
+        Configuration.GetSection("Backplane").Bind(_backplaneOptions);
+
         _swaggerOptions = new();
         Configuration.GetSection("Swagger").Bind(_swaggerOptions);
     }
@@ -53,18 +61,28 @@ public class Startup : StartupBase
             .AddNpgsql(
                 "config-db",
                 ConfigDbOptions.Connect,
-                tags: new[] { HealthTags.Health, HealthTags.Ready });
+                tags: new[] { HealthTags.Health, HealthTags.Ready })
+            .AddKafka(
+                "backplane",
+                _backplaneOptions.Kafka,
+                _backplaneOptions.Health,
+                tags: new[] { HealthTags.Health });
     }
 
     public void ConfigureContainer(ContainerBuilder builder)
     {
         // ordered hosted services
         builder.RegisterHostedService<InitConfigDb>();
+        builder.RegisterHostedService<InitBackplane>();
 
         // config db
         builder.RegisterType<NpgsqlDbInitializer>().As<INpgsqlDbInitializer>().SingleInstance();
         IConfiguration configDbConfig = Configuration.GetSection("ConfigDb");
         builder.RegisterOptions<ConfigDbOptions>(configDbConfig);
+
+        // backplane
+        builder.RegisterType<KafkaAdmin>().As<IKafkaAdmin>().SingleInstance();
+        builder.RegisterOptions<KafkaOptions>(Configuration.GetSection("Backplane:Kafka"));
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
