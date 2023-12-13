f1308b8a4980daca0c840227f5c1b517a5385dd1
ceco <cvetomir.todorov@protonmail.com>
2023-12-13 12:04:07 +0200

    Replace redis-cli config data generation scripts with ones based on Postgres for both docker and minikube deployments

---
+++ deploy/docker/yugabyte/config.sql
@@ -0,0 +1,17 @@
+DO
+$body$
+    DECLARE
+        version timestamp with time zone;
+    BEGIN
+        version := current_timestamp;
+
+        -- partitioning
+        INSERT INTO "Elements" VALUES ('partitioning.count', '12', version);
+        INSERT INTO "Elements" VALUES ('partitioning.partitions', '0=0-5;1=6-11', version);
+        INSERT INTO "Elements" VALUES ('partitioning.addresses', '0=https://localhost:31000;1=https://localhost:31001', version);
+        -- history
+        INSERT INTO "Elements" VALUES ('history.message-count', '32', version);
+        -- snowflake
+        INSERT INTO "Elements" VALUES ('snowflake.generator-ids', '0=0,1,2,3', version);
+    END
+$body$
---
+++ deploy/minikube/yugabyte/config.sql
@@ -0,0 +1,17 @@
+DO
+$body$
+    DECLARE
+        version timestamp with time zone;
+    BEGIN
+        version := current_timestamp;
+
+        -- partitioning
+        INSERT INTO "Elements" VALUES ('partitioning.count', '12', version);
+        INSERT INTO "Elements" VALUES ('partitioning.partitions', '0=0-5;1=6-11', version);
+        INSERT INTO "Elements" VALUES ('partitioning.addresses', '0=https://messaging.cecochat.com/m0;1=https://messaging.cecochat.com/m1', version);
+        -- history
+        INSERT INTO "Elements" VALUES ('history.message-count', '32', version);
+        -- snowflake
+        INSERT INTO "Elements" VALUES ('snowflake.generator-ids', '0=0,1;1=2,3', version);
+    END
+$body$
