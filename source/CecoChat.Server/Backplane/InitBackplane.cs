1d851938b28cc87d5ec25ac2b1dcf6f01d3f9d6a
ceco <cvetomir.todorov@protonmail.com>
2023-12-13 17:04:23 +0200

    Create a topic for config changes

--- source/CecoChat.Server/Backplane/InitBackplane.cs
+++ source/CecoChat.Server/Backplane/InitBackplane.cs
@@ -17,6 +17,7 @@ public class InitBackplane : IHostedService
         KafkaTopicSpec[] topics =
         {
             new("messages-by-receiver", partitionCount: 12, replicationFactor: 2, minInSyncReplicas: 2),
+            new("config-changes", partitionCount: 12, replicationFactor: 2, minInSyncReplicas: 2),
             new("health", partitionCount: 1, replicationFactor: 1)
         };
 
