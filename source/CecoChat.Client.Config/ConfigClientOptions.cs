5f8854cfcdab20376c205a3db8413f9aa34e384e
ceco <cvetomir.todorov@protonmail.com>
2023-12-14 18:13:12 +0200

    Create a config client

---
+++ source/CecoChat.Client.Config/ConfigClientOptions.cs
@@ -0,0 +1,16 @@
+using CecoChat.Http.Client;
+using CecoChat.Polly;
+
+namespace CecoChat.Client.Config;
+
+public sealed class ConfigClientOptions
+{
+    public Uri? Address { get; init; }
+    public TimeSpan CallTimeout { get; init; }
+
+    public SocketsHttpHandlerOptions? SocketsHttpHandler { get; init; }
+    public RetryOptions? Retry { get; init; }
+
+    public string HealthPath { get; init; } = string.Empty;
+    public TimeSpan HealthTimeout { get; init; }
+}
