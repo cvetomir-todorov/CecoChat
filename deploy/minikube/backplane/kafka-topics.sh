b045efccda47d0bf6e7f44bbba97dc3f87948531
ceco <cvetomir.todorov@protonmail.com>
2023-12-14 10:41:05 +0200

    Change kafka topic config-changes partition count to just 1

--- deploy/minikube/backplane/kafka-topics.sh
+++ deploy/minikube/backplane/kafka-topics.sh
@@ -1,4 +1,4 @@
 kafka-topics --bootstrap-server backplane-kafka-0.backplane-kafka.cecochat.svc.cluster.local:9092 --create --topic messages-by-receiver --partitions 12 --replication-factor 2 --config min.insync.replicas=2
-kafka-topics --bootstrap-server backplane-kafka-0.backplane-kafka.cecochat.svc.cluster.local:9092 --create --topic config-changes --partitions 12 --replication-factor 2 --config min.insync.replicas=2
+kafka-topics --bootstrap-server backplane-kafka-0.backplane-kafka.cecochat.svc.cluster.local:9092 --create --topic config-changes --partitions 1 --replication-factor 2 --config min.insync.replicas=2
 kafka-topics --bootstrap-server backplane-kafka-0.backplane-kafka.cecochat.svc.cluster.local:9092 --create --topic health --partitions 1 --replication-factor 1
 kafka-topics --bootstrap-server backplane-kafka-0.backplane-kafka.cecochat.svc.cluster.local:9092 --list
