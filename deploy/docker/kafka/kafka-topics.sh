717380ba8e68115bce808275b346599294d16ac9
ceco <cvetomir.todorov@protonmail.com>
2023-12-14 10:41:05 +0200

    Change kafka topic config-changes partition count to just 1

--- deploy/docker/kafka/kafka-topics.sh
+++ deploy/docker/kafka/kafka-topics.sh
@@ -1,4 +1,4 @@
 kafka-topics --bootstrap-server kafka0:9091 --create --topic messages-by-receiver --partitions 12 --replication-factor 2 --config min.insync.replicas=2
-kafka-topics --bootstrap-server kafka0:9091 --create --topic config-changes --partitions 12 --replication-factor 2 --config min.insync.replicas=2
+kafka-topics --bootstrap-server kafka0:9091 --create --topic config-changes --partitions 1 --replication-factor 2 --config min.insync.replicas=2
 kafka-topics --bootstrap-server kafka0:9091 --create --topic health --partitions 1 --replication-factor 1
 kafka-topics --bootstrap-server kafka0:9091 --list
