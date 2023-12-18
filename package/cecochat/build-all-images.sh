94fed6036dcb44f0dadfabc88f51be1a3043fd60
ceco <cvetomir.todorov@protonmail.com>
2023-12-18 17:59:55 +0200

    Create a dockerfile for Config service

--- package/cecochat/build-all-images.sh
+++ package/cecochat/build-all-images.sh
@@ -3,3 +3,4 @@ docker build -f messaging.dockerfile -t ceco.com/cecochat/messaging:0.1 ../../so
 docker build -f chats.dockerfile -t ceco.com/cecochat/chats:0.1 ../../source/
 docker build -f idgen.dockerfile -t ceco.com/cecochat/idgen:0.1 ../../source/
 docker build -f user.dockerfile -t ceco.com/cecochat/user:0.1 ../../source/
+docker build -f config.dockerfile -t ceco.com/cecochat/config:0.1 ../../source/
