897903f1411ad6d421311a8bbb13b4054328919f
ceco <cvetomir.todorov@protonmail.com>
2023-12-30 15:02:50 +0200

    Make file controller private methods more clear

--- source/CecoChat.Server.Bff/Endpoints/Files/FileController.cs
+++ source/CecoChat.Server.Bff/Endpoints/Files/FileController.cs
@@ -59,10 +59,10 @@ public class FileController : ControllerBase
             return uploadFileResult.Failure;
         }
 
-        (DateTime fileVersion, IActionResult? failure) = await TryAddFileForUser(userClaims, uploadFileResult.Bucket, uploadFileResult.Path, accessToken, ct);
-        if (failure != null)
+        AssociateFileResult associateFileResult = await AssociateFile(userClaims, uploadFileResult.Bucket, uploadFileResult.Path, accessToken, ct);
+        if (associateFileResult.Failure != null)
         {
-            return failure;
+            return associateFileResult.Failure;
         }
 
         UploadFileResponse response = new();
@@ -70,7 +70,7 @@ public class FileController : ControllerBase
         {
             Bucket = uploadFileResult.Bucket,
             Path = uploadFileResult.Path,
-            Version = fileVersion
+            Version = associateFileResult.FileVersion
         };
 
         return Ok(response);
@@ -78,9 +78,9 @@ public class FileController : ControllerBase
 
     private struct UploadFileResult
     {
-        public string Bucket { get; set; }
-        public string Path { get; set; }
-        public IActionResult? Failure { get; set; }
+        public string Bucket { get; init; }
+        public string Path { get; init; }
+        public IActionResult? Failure { get; init; }
     }
 
     private async Task<UploadFileResult> UploadFile(UserClaims userClaims, string? contentType, Stream body, long fileSize, CancellationToken ct)
@@ -123,22 +123,36 @@ public class FileController : ControllerBase
         };
     }
 
-    private async Task<(DateTime, IActionResult?)> TryAddFileForUser(UserClaims userClaims, string bucket, string path, string accessToken, CancellationToken ct)
+    private struct AssociateFileResult
+    {
+        public DateTime FileVersion { get; init; }
+        public IActionResult? Failure { get; init; }
+    }
+
+    private async Task<AssociateFileResult> AssociateFile(UserClaims userClaims, string bucket, string path, string accessToken, CancellationToken ct)
     {
         AddFileResult result = await _fileClient.AddFile(userClaims.UserId, bucket, path, accessToken, ct);
 
         if (result.Success)
         {
             _logger.LogTrace("Associated successfully a new file in bucket {Bucket} with path {Path} and user {UserId}", bucket, path, userClaims.UserId);
-            return (result.Version, null);
+            return new AssociateFileResult
+            {
+                FileVersion = result.Version
+            };
         }
         if (result.DuplicateFile)
         {
             _logger.LogTrace("Association failed for a duplicate file in bucket {Bucket} with path {Path} and user {UserId}", bucket, path, userClaims.UserId);
-            return (DateTime.MinValue, Conflict(new ProblemDetails
+            IActionResult failure = Conflict(new ProblemDetails
             {
                 Detail = "Duplicate file"
-            }));
+            });
+
+            return new AssociateFileResult
+            {
+                Failure = failure
+            };
         }
 
         throw new ProcessingFailureException(typeof(AddFileResult));
