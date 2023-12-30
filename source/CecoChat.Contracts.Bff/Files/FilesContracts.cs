5aa4cf916367cf968fd8a58d3b606206da93594a
ceco <cvetomir.todorov@protonmail.com>
2023-12-30 14:58:36 +0200

    Change UploadFileResponse to use the FileRef object

--- source/CecoChat.Contracts.Bff/Files/FilesContracts.cs
+++ source/CecoChat.Contracts.Bff/Files/FilesContracts.cs
@@ -3,20 +3,11 @@ using Refit;
 
 namespace CecoChat.Contracts.Bff.Files;
 
-// TODO: encapsulate properties into a single File object
 public sealed class UploadFileResponse
 {
-    [JsonPropertyName("bucket")]
-    [AliasAs("bucket")]
-    public string Bucket { get; init; } = string.Empty;
-
-    [JsonPropertyName("path")]
-    [AliasAs("path")]
-    public string Path { get; init; } = string.Empty;
-
-    [JsonPropertyName("version")]
-    [AliasAs("version")]
-    public DateTime Version { get; init; }
+    [JsonPropertyName("file")]
+    [AliasAs("file")]
+    public FileRef File { get; set; } = new();
 }
 
 public sealed class FileRef
