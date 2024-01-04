7e649a9b72cfc813ec4e47917d7bca0a464ceea2
ceco <cvetomir.todorov@protonmail.com>
2024-01-04 10:28:51 +0200

    Remove unused parameter

--- source/CecoChat.Server.Config/Program.cs
+++ source/CecoChat.Server.Config/Program.cs
@@ -41,7 +41,7 @@ public static class Program
         _swaggerOptions = new();
         builder.Configuration.GetSection("Swagger").Bind(_swaggerOptions);
 
-        AddServices(builder, options);
+        AddServices(builder);
         AddTelemetry(builder, options);
         AddHealth(builder);
         builder.Host.ConfigureContainer<ContainerBuilder>(ConfigureContainer);
@@ -51,7 +51,7 @@ public static class Program
         await EntryPoint.RunWebApp(app, typeof(Program));
     }
 
-    private static void AddServices(WebApplicationBuilder builder, CommonOptions options)
+    private static void AddServices(WebApplicationBuilder builder)
     {
         // grpc
         builder.Services.AddGrpc(grpc =>
