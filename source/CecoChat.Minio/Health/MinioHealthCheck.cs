b257ec1395c90fa2c3547244f17bb6c81326248d
ceco <cvetomir.todorov@protonmail.com>
2024-01-08 17:43:19 +0200

    Add a MinIO health check

---
+++ source/CecoChat.Minio/Health/MinioHealthCheck.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Minio;
+using Minio.DataModel.Args;
+
+namespace CecoChat.Minio.Health;
+
+/// <summary>
+/// Checks the health of MinIO by checking if a bucket exists.
+/// </summary>
+public class MinioHealthCheck : IHealthCheck
+{
+    private readonly ILogger _logger;
+    private readonly IMinioClient _minio;
+    private readonly string _bucket;
+
+    public MinioHealthCheck(
+        ILogger<MinioHealthCheck> logger,
+        IMinioClient minio,
+        string bucket)
+    {
+        _logger = logger;
+        _minio = minio;
+        _bucket = bucket;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            BucketExistsArgs bucketExistsArgs = new BucketExistsArgs()
+                .WithBucket(_bucket);
+            bool _ = await _minio.BucketExistsAsync(bucketExistsArgs, cancellationToken);
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "MinIO health check {HealthCheckName} failed", context.Registration.Name);
+            return new HealthCheckResult(HealthStatus.Unhealthy, exception.Message, exception);
+        }
+    }
+}
--- source/CecoChat.Minio/MinioOptions.cs
+++ source/CecoChat.Minio/MinioOptions.cs
@@ -5,4 +5,7 @@ public sealed class MinioOptions
     public Uri Endpoint { get; init; } = null!;
     public string AccessKey { get; init; } = string.Empty;
     public string Secret { get; init; } = string.Empty;
+
+    public string HealthBucket { get; init; } = string.Empty;
+    public TimeSpan HealthTimeout { get; init; }
 }
