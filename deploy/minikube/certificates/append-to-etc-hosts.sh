b13dfee1961b6131a6efbe7f833eef83e185d0d3
ceco <cvetomir.todorov@protonmail.com>
2024-01-14 14:48:26 +0200

    Add MinIO ingress to minikube deployment

--- deploy/minikube/certificates/append-to-etc-hosts.sh
+++ deploy/minikube/certificates/append-to-etc-hosts.sh
@@ -5,3 +5,4 @@ echo "$MINIKUBE_IP messaging.cecochat.com" | sudo tee --append /etc/hosts
 echo "$MINIKUBE_IP jaeger.cecochat.com" | sudo tee --append /etc/hosts
 echo "$MINIKUBE_IP kibana.cecochat.com" | sudo tee --append /etc/hosts
 echo "$MINIKUBE_IP config.cecochat.com" | sudo tee --append /etc/hosts
+echo "$MINIKUBE_IP minio.cecochat.com" | sudo tee --append /etc/hosts
