bc86dffff8231425f6d971878486af67a9ebd1c6
ceco <cvetomir.todorov@protonmail.com>
2024-01-14 15:32:45 +0200

    Update script to (un)install MinIO to minikube deployment

--- deploy/minikube/install-data-integration.sh
+++ deploy/minikube/install-data-integration.sh
@@ -2,3 +2,4 @@ helm install redis redis/
 helm install backplane backplane/
 helm install cassandra cassandra/
 helm install yb yugabyte/
+helm install minio minio/
--- deploy/minikube/uninstall-data-integration.sh
+++ deploy/minikube/uninstall-data-integration.sh
@@ -2,3 +2,4 @@ helm uninstall redis
 helm uninstall backplane
 helm uninstall cassandra
 helm uninstall yb
+helm uninstall minio
