67a7d16ade7818cde8e219c47418c1c280ee693d
ceco <cvetomir.todorov@protonmail.com>
2024-01-17 16:56:01 +0200

    Return a limited number of profiles when searching by user name in User service

--- source/CecoChat.Server.User/Init/DynamicConfigInit.cs
+++ source/CecoChat.Server.User/Init/DynamicConfigInit.cs
@@ -1,26 +1,32 @@
 using CecoChat.AspNet.Init;
 using CecoChat.DynamicConfig.Partitioning;
+using CecoChat.DynamicConfig.User;
 
 namespace CecoChat.Server.User.Init;
 
 public class DynamicConfigInit : InitStep
 {
     private readonly IPartitioningConfig _partitioningConfig;
+    private readonly IUserConfig _userConfig;
     private readonly DynamicConfigInitHealthCheck _dynamicConfigInitHealthCheck;
 
     public DynamicConfigInit(
         IPartitioningConfig partitioningConfig,
+        IUserConfig userConfig,
         DynamicConfigInitHealthCheck dynamicConfigInitHealthCheck,
         IHostApplicationLifetime applicationLifetime)
         : base(applicationLifetime)
     {
         _partitioningConfig = partitioningConfig;
+        _userConfig = userConfig;
         _dynamicConfigInitHealthCheck = dynamicConfigInitHealthCheck;
     }
 
     protected override async Task<bool> DoExecute(CancellationToken ct)
     {
-        _dynamicConfigInitHealthCheck.IsReady = await _partitioningConfig.Initialize(ct);
+        _dynamicConfigInitHealthCheck.IsReady =
+            await _partitioningConfig.Initialize(ct) &&
+            await _userConfig.Initialize(ct);
 
         return _dynamicConfigInitHealthCheck.IsReady;
     }
