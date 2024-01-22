404096bb1c15cd20cdc0710ec34d2cb25d5db3c9
ceco <cvetomir.todorov@protonmail.com>
2024-01-22 22:26:16 +0200

    Add file message support to Messaging service

---
+++ source/CecoChat.Server.Messaging/Endpoints/ChatHub.Common.cs
@@ -0,0 +1,19 @@
+using CecoChat.Client.IdGen;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CecoChat.Server.Messaging.Endpoints;
+
+public partial class ChatHub
+{
+    private async Task<long> GetMessageId()
+    {
+        // SignalR doesn't support yet CancellationToken in the method signature so we have none
+        GetIdResult result = await _idGenClient.GetId(CancellationToken.None);
+        if (!result.Success)
+        {
+            throw new HubException("Failed to obtain a message ID.");
+        }
+
+        return result.Id;
+    }
+}
