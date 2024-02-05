3a1d0de44c3c08bf6ceaa2c0b76881bbf525821b
ceco <cvetomir.todorov@protonmail.com>
2024-02-05 21:17:32 +0200

    Change UserDB scripts - table, column, constraint, index names use lowercase_underscore naming

--- source/CecoChat.Data.User/Scripts/table-02-connections.sql
+++ source/CecoChat.Data.User/Scripts/table-02-connections.sql
@@ -1,11 +1,16 @@
-CREATE TABLE IF NOT EXISTS public."Connections"
+CREATE TABLE IF NOT EXISTS public.connections
 (
-    "User1Id" bigint NOT NULL,
-    "User2Id" bigint NOT NULL,
-    "Version" timestamp with time zone NOT NULL,
-    "Status" text COLLATE pg_catalog."default" NOT NULL,
-    "TargetId" bigint NOT NULL,
-    CONSTRAINT "Connections_pkey" PRIMARY KEY ("User1Id", "User2Id")
+    user1_id bigint NOT NULL,
+    user2_id bigint NOT NULL,
+    version timestamp with time zone NOT NULL,
+    status text COLLATE pg_catalog."default" NOT NULL,
+    target_id bigint NOT NULL,
+    CONSTRAINT connections_pkey
+        PRIMARY KEY (user1_id, user2_id),
+    CONSTRAINT connections_user1_id_foreign
+        FOREIGN KEY (user1_id) REFERENCES public.profiles (user_id),
+    CONSTRAINT connections_user2_id_foreign
+        FOREIGN KEY (user2_id) REFERENCES public.Profiles (user_id)
 )
 WITH
 (
@@ -13,23 +18,15 @@ WITH
 )
 TABLESPACE pg_default;
 
-CREATE INDEX "Connections_User1Id_index"
-    ON public."Connections" USING btree
-    ("User1Id" ASC NULLS LAST);
+CREATE INDEX connections_user1_id_index
+    ON public.connections USING btree
+        (user1_id ASC NULLS LAST);
 
-CREATE INDEX "Connections_User2Id_index"
-    ON public."Connections" USING btree
-        ("User2Id" ASC NULLS LAST);
+CREATE INDEX connections_user2_id_index
+    ON public.connections USING btree
+        (user2_id ASC NULLS LAST);
 
-ALTER TABLE IF EXISTS public."Connections"
-    ADD CONSTRAINT "Connections_User1Id_foreign"
-        FOREIGN KEY ("User1Id") REFERENCES public."Profiles"("UserId");
-
-ALTER TABLE IF EXISTS public."Connections"
-    ADD CONSTRAINT "Connections_User2Id_foreign"
-        FOREIGN KEY ("User2Id") REFERENCES public."Profiles"("UserId");
-
-ALTER TABLE IF EXISTS public."Connections"
+ALTER TABLE IF EXISTS public.connections
     OWNER to yugabyte;
-GRANT ALL ON TABLE public."Connections" TO cecochat_dev;
-GRANT ALL ON TABLE public."Connections" TO yugabyte;
+GRANT ALL ON TABLE public.connections TO cecochat_dev;
+GRANT ALL ON TABLE public.connections TO yugabyte;
