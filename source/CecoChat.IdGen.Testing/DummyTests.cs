946f77e11488b924ce0ebf9238138becbad4528d
ceco <cvetomir.todorov@protonmail.com>
2024-05-16 18:29:33 +0300

    Add a new step in the CI pipeline to run tests

---
+++ source/CecoChat.IdGen.Testing/DummyTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace CecoChat.IdGen.Testing;
+
+public class DummyTests
+{
+    [Test]
+    public void Dummy()
+    {
+        throw new Exception("haha");
+    }
+}
