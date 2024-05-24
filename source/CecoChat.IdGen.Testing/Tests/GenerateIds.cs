cc4568adc8b1e4257c08ebf3c62f3263fb70370b
ceco <cvetomir.todorov@protonmail.com>
2024-05-24 17:41:54 +0300

    Add more test cases for generating IDs and set ID Gen test client ID wait interval to its initial value of 2s

--- source/CecoChat.IdGen.Testing/Tests/GenerateIds.cs
+++ source/CecoChat.IdGen.Testing/Tests/GenerateIds.cs
@@ -10,11 +10,14 @@ public class GenerateIds : BaseTest
 {
     // client is configured to refresh IDs by 16 each second
     [TestCase(1)]
-    //[TestCase(2)]
-    //[TestCase(16)]
+    [TestCase(2)]
+    [TestCase(16)]
     // should force a new call
-    //[TestCase(32)]
-    public async Task GenerateNumberOfIds(int idCount)
+    [TestCase(32)]
+    [TestCase(48)]
+    [TestCase(64)]
+    [TestCase(128)]
+    public async Task Consecutively(int idCount)
     {
         List<long> ids = new(capacity: idCount);
         DateTime start = DateTime.UtcNow;
@@ -37,6 +40,55 @@ public class GenerateIds : BaseTest
             {
                 DateTime idDateTime = id.ToTimestamp();
                 idDateTime.Should().BeWithin(TimeSpan.FromSeconds(2.5)).After(start.AddSeconds(-0.5));
+            }
+        }
+    }
+
+    // client is configured to refresh IDs by 16 each second
+    [TestCase(2, 1)]
+    [TestCase(4, 4)]
+    [TestCase(16, 1)]
+    // should force a new call
+    [TestCase(16, 2)]
+    [TestCase(16, 4)]
+    [TestCase(16, 8)]
+    [TestCase(16, 16)]
+    [TestCase(16, 32)]
+    public async Task Simultaneously(int parallelCount, int idCountPerParallel)
+    {
+        IEnumerable<int> parallels = Enumerable.Range(0, parallelCount);
+        Dictionary<int, List<long>> ids = new(capacity: parallelCount);
+        DateTime start = DateTime.UtcNow;
+
+        await Parallel.ForEachAsync(parallels, async (parallel, ct) =>
+        {
+            int localParallel = parallel;
+            ids[localParallel] = new List<long>();
+
+            for (int i = 0; i < idCountPerParallel; ++i)
+            {
+                GetIdResult result = await Client.GetId(ct);
+                if (result.Success)
+                {
+                    ids[localParallel].Add(result.Id);
+                }
+            }
+        });
+
+        using (new AssertionScope())
+        {
+            foreach (KeyValuePair<int,List<long>> pair in ids)
+            {
+                List<long> idsForCurrentParallel = pair.Value;
+
+                idsForCurrentParallel.Count.Should().Be(idCountPerParallel);
+                idsForCurrentParallel.Should().BeInAscendingOrder();
+
+                foreach (long id in idsForCurrentParallel)
+                {
+                    DateTime idDateTime = id.ToTimestamp();
+                    idDateTime.Should().BeWithin(TimeSpan.FromSeconds(2.5)).After(start.AddSeconds(-0.5));
+                }
             }
         }
     }
