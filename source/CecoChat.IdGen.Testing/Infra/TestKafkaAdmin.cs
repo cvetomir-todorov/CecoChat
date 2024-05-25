18ac65a470ff8ad00f8b1292bb8206fd70754112
ceco <cvetomir.todorov@protonmail.com>
2024-05-25 19:10:40 +0300

    Disable warning for unused parameters which are actually part of the interface

--- source/CecoChat.IdGen.Testing/Infra/TestKafkaAdmin.cs
+++ source/CecoChat.IdGen.Testing/Infra/TestKafkaAdmin.cs
@@ -4,7 +4,9 @@ namespace CecoChat.IdGen.Testing.Infra;
 
 public class TestKafkaAdmin : IKafkaAdmin
 {
+#pragma warning disable IDE0060 // Remove unused parameter
     public Task CreateTopics(IEnumerable<KafkaTopicSpec> topics)
+#pragma warning restore IDE0060 // Remove unused parameter
     {
         return Task.CompletedTask;
     }
--- source/CecoChat.IdGen.Testing/Infra/TestKafkaConsumer.cs
+++ source/CecoChat.IdGen.Testing/Infra/TestKafkaConsumer.cs
@@ -8,15 +8,23 @@ public class TestKafkaConsumer<TKey, TValue> : IKafkaConsumer<TKey, TValue>
     public void Dispose()
     { }
 
+#pragma warning disable IDE0060 // Remove unused parameter
     public void Initialize(KafkaOptions options, KafkaConsumerOptions consumerOptions, IDeserializer<TValue> valueDeserializer)
+#pragma warning restore IDE0060 // Remove unused parameter
     { }
 
+#pragma warning disable IDE0060 // Remove unused parameter
     public void Subscribe(string topic)
+#pragma warning restore IDE0060 // Remove unused parameter
     { }
 
+#pragma warning disable IDE0060 // Remove unused parameter
     public void Assign(string topic, PartitionRange partitions, ITopicPartitionFlyweight partitionFlyweight)
+#pragma warning restore IDE0060 // Remove unused parameter
     { }
 
+#pragma warning disable IDE0060 // Remove unused parameter
     public void Consume(Action<ConsumeResult<TKey, TValue>> messageHandler, CancellationToken ct)
+#pragma warning restore IDE0060 // Remove unused parameter
     { }
 }
