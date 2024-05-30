cb4c8e2a935f9b1fd8a4211992b632bfa38f0971
ceco <cvetomir.todorov@protonmail.com>
2024-05-30 21:30:49 +0300

    Move NUnit progress logger to the Common.Testing project

--- source/CecoChat.Chats.Testing/TestContainers.cs
+++ source/CecoChat.Chats.Testing/TestContainers.cs
@@ -1,8 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
 using Cassandra;
-using Common;
 using Common.Cassandra;
+using Common.Testing.Logging;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
@@ -86,48 +86,6 @@ public class TestContainers
 
         using CassandraDbContext db = new(new NUnitProgressLogger<CassandraDbContext>(), new OptionsWrapper<CassandraOptions>(options));
         TestContext.Progress.WriteLine("Exists keyspace? {0}", db.ExistsKeyspace("unknown"));
-    }
-}
-
-public class NUnitProgressLogger<TCategoryName> : ILogger<TCategoryName>
-{
-#pragma warning disable IDE0060
-    public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
-#pragma warning restore IDE0060
-
-#pragma warning disable IDE0060
-    public bool IsEnabled(LogLevel logLevel) => true;
-#pragma warning restore IDE0060
-
-    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-    {
-        string message = formatter.Invoke(state, exception);
-        string level;
-        switch (logLevel)
-        {
-            case LogLevel.Trace:
-                level = "VRB";
-                break;
-            case LogLevel.Debug:
-                level = "DBG";
-                break;
-            case LogLevel.Information:
-                level = "INF";
-                break;
-            case LogLevel.Warning:
-                level = "WRN";
-                break;
-            case LogLevel.Error:
-                level = "ERR";
-                break;
-            case LogLevel.Critical:
-                level = "CRI";
-                break;
-            default:
-                throw new EnumValueNotSupportedException(logLevel);
-        }
-
-        TestContext.Progress.WriteLine("[{0:HH:mm:ss.fff} {1}] {2} {3}", DateTime.UtcNow, level, message, exception);
     }
 }
 
---
+++ source/Common.Testing/Logging/NUnitProgressLogger.cs
@@ -0,0 +1,46 @@
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace Common.Testing.Logging;
+
+public class NUnitProgressLogger<TCategoryName> : ILogger<TCategoryName>
+{
+#pragma warning disable IDE0060
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
+#pragma warning restore IDE0060
+
+#pragma warning disable IDE0060
+    public bool IsEnabled(LogLevel logLevel) => true;
+#pragma warning restore IDE0060
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        string message = formatter.Invoke(state, exception);
+        string level;
+        switch (logLevel)
+        {
+            case LogLevel.Trace:
+                level = "VRB";
+                break;
+            case LogLevel.Debug:
+                level = "DBG";
+                break;
+            case LogLevel.Information:
+                level = "INF";
+                break;
+            case LogLevel.Warning:
+                level = "WRN";
+                break;
+            case LogLevel.Error:
+                level = "ERR";
+                break;
+            case LogLevel.Critical:
+                level = "CRI";
+                break;
+            default:
+                throw new EnumValueNotSupportedException(logLevel);
+        }
+
+        TestContext.Progress.WriteLine("[{0:HH:mm:ss.fff} {1}] {2} {3}", DateTime.UtcNow, level, message, exception);
+    }
+}
